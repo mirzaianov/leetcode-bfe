1bb4933e83737912d5b372d430325c94af0fd94b
kirsan <7o.msejak@gmail.com>
2023-01-09 19:02:09 +0300

    1812

---
+++ 1812. Determine Color of a Chessboard Square/script.js
@@ -0,0 +1,18 @@
+const squareIsWhite = function (coordinates) {
+    let coordinates2Array = coordinates.toString().split('');
+
+    const hor = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+        ver = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+    let indexOfHor = hor.indexOf(coordinates2Array[0]) + 1;
+    console.log(indexOfHor);
+
+    let indexOfVer = ver.indexOf(coordinates2Array[1]) + 1;
+    console.log(indexOfVer);
+
+    return (indexOfHor + indexOfVer) % 2 !== 0;
+};
+
+console.log(squareIsWhite('a1')); // false
+console.log(squareIsWhite('h3')); // true
+console.log(squareIsWhite('c7')); // false
