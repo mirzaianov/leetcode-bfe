2b5138002fdab078ae702c0673d8535707e5d628
kirsan <7o.msejak@gmail.com>
2023-01-15 18:59:41 +0300

    leetcode-1365

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number/script.js
@@ -0,0 +1,13 @@
+const smallerNumbersThanCurrent = function (nums) {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums.filter((item) => item < nums[i]).length);
+    }
+
+    return result;
+};
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3])); // [4, 0, 1, 1, 3]
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8])); // [2, 1, 0, 3]
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7])); // [0, 0, 0, 0]
