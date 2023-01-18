a595212007056caf7fecb38da58e69620db97a5a
kirsan <7o.msejak@gmail.com>
2023-01-18 12:47:56 +0300

    leetcode-1588-2

--- 1588. Sum of All Odd Length Subarrays/script.js
+++ 1588. Sum of All Odd Length Subarrays/script.js
@@ -1,19 +1,36 @@
+// const sumOddLengthSubarrays = function (arr) {
+//     let i = 1;
+//     let sum = 0;
+
+//     while (i <= arr.length) {
+//         for (let j = 0; j <= arr.length - i; j++) {
+//             for (let k = j; k < i + j; k++) {
+//                 sum += arr[k];
+//                 console.log(`[i] ${i}, [j] ${j}, [k] ${k}, [arr[k]] ${arr[k]}, [sum] ${sum}`);
+//             }
+//         }
+//         i += 2;
+//     }
+
+//     return sum;
+// };
+
 const sumOddLengthSubarrays = function (arr) {
     let i = 1;
     let sum = 0;
 
-    while (i <= arr.length) {
+    for (i = 1; i <= arr.length; i += 2) {
         for (let j = 0; j <= arr.length - i; j++) {
             for (let k = j; k < i + j; k++) {
                 sum += arr[k];
+                console.log(`[i] ${i}, [j] ${j}, [k] ${k}, [arr[k]] ${arr[k]}, [sum] ${sum}`);
             }
         }
-        i += 2;
     }
 
     return sum;
 };
 
 console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3])); //58
-console.log(sumOddLengthSubarrays([1, 2])); // 3
-console.log(sumOddLengthSubarrays([10, 11, 12])); // 66
+// console.log(sumOddLengthSubarrays([1, 2])); // 3
+// console.log(sumOddLengthSubarrays([10, 11, 12])); // 66
