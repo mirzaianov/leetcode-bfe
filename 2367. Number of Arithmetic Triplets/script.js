492fb69ea98ef75ae41f9f61c02e9b3bf7a6b5e7
kirsan <7o.msejak@gmail.com>
2023-01-18 11:23:33 +0300

    leetcode-2367

---
+++ 2367. Number of Arithmetic Triplets/script.js
@@ -0,0 +1,30 @@
+// const arithmeticTriplets = function (nums, diff) {
+//     let arr = [];
+
+//     for (let i = 0; i <= nums.length - 3; i++) {
+//         for (let j = i + 1; j <= nums.length - 2; j++) {
+//             for (let k = j + 1; k <= nums.length - 1; k++) {
+//                 if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) {
+//                     arr.push([nums[i], nums[j], nums[k]]);
+//                 }
+//             }
+//         }
+//     }
+
+//     return arr.length;
+// };
+
+const arithmeticTriplets = function (nums, diff) {
+    let count = 0;
+
+    for (let i of nums) {
+        if (nums.includes(i + diff) && nums.includes(i + diff * 2)) {
+            count++;
+        }
+    }
+
+    return count;
+};
+
+console.log(arithmeticTriplets([0, 1, 4, 6, 7, 10], 3)); // 2
+console.log(arithmeticTriplets([4, 5, 6, 7, 8, 9], 2)); // 2
