ac72cb8402ed451ed9201450bf7c5ad01b25721d
msejak <7o.mseJak@gmail.com>
2023-02-06 10:47:15 +0300

    leetcode 2006

---
+++ 2006. Count Number of Pairs With Absolute Difference K/script.js
@@ -0,0 +1,17 @@
+const countKDifference = function (nums, k) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (Math.abs(nums[i] - nums[j]) === k) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+};
+
+console.log(countKDifference([1, 2, 2, 1], 1)); // 4
+console.log(countKDifference([1, 3], 3)); // 0
+console.log(countKDifference([3, 2, 1, 5, 4], 2)); // 3
