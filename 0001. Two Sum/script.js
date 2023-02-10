8e17b0c081833a9714e4878481de1d964d4c7ea9
msejak <7o.mseJak@gmail.com>
2023-02-10 17:17:33 +0300

    leetcode 0001-02

--- 0001. Two Sum/script.js
+++ 0001. Two Sum/script.js
@@ -1,11 +1,46 @@
-const twoSum = function (nums, target) {
+// const twoSum = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
+
+function twoSum(nums, target) {
+    const map = {};
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        const element = nums[i];
+        const complement = target - element;
+
+        if (map[complement] !== undefined) {
+            return [map[complement], i];
+        } else {
+            map[element] = i;
         }
     }
-};
 
-console.log(twoSum([2, 7, 11, 15], 26));
+    return [];
+}
+
+// const twoSum = function (nums, target) {
+//     const map = new Map();
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const match = map.get(target - nums[i]);
+
+//         if (match !== undefined) {
+//             return [match, i];
+//         }
+
+//         map.set(nums[i], i);
+//     }
+
+//     return [];
+// };
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // [1, 2]
+console.log(twoSum([3, 3], 6)); // [0, 1]
