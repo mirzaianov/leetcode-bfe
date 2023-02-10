2bf30fc5cb437af019b2fe5225ac566b60ba5e77
msejak <7o.mseJak@gmail.com>
2023-02-10 11:06:52 +0300

    leetcode 0217

---
+++ 0217. Contains Duplicate/script.js
@@ -0,0 +1,16 @@
+const containsDuplicate = function (nums) {
+    const newSet = new Set();
+
+    for (let item of nums) {
+        if (newSet.has(item)) {
+            return true;
+        }
+        newSet.add(item);
+    }
+
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
