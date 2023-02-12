4999cdf84ab7e0714bf632def7cd37e9185ebbde
msejak <7o.mseJak@gmail.com>
2023-02-12 13:47:43 +0300

    leetcode 0070

---
+++ 0070. Climbing Stairs/script.js
@@ -0,0 +1,43 @@
+// const climbStairs = function (n) {
+//     if (n === 0) {
+//         return 0;
+//     } else if (n < 3) {
+//         return n;
+//     } else {
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
+
+// const climbStairs = function (n) {
+//     if (n === 1) {
+//         return 1;
+//     }
+
+//     n1 = 1;
+//     n2 = 2;
+
+//     for (let i = 3; i < n + 1; i++) {
+//         [n1, n2] = [n2, n1 + n2];
+//     }
+
+//     return n2;
+// };
+
+const climbStairs = function (n) {
+    let memo = {};
+
+    return count(n);
+
+    function count(k) {
+        if (memo[k]) return memo[k];
+        if (k === 2) return 2;
+        if (k <= 1) return 1;
+
+        memo[k] = count(k - 1) + count(k - 2);
+        return memo[k];
+    }
+};
+
+// console.log(climbStairs(2)); // 2
+// console.log(climbStairs(3)); // 3
+console.log(climbStairs(5)); // 8
