9087cadc295b25d35bba267b731dba5edc3c95ea
msejak <7o.mseJak@gmail.com>
2023-02-19 12:55:32 +0300

    leetcode 0344

---
+++ 0344. Reverse String/script.js
@@ -0,0 +1,15 @@
+const reverseString = function (s) {
+    let l = 0;
+    r = s.length - 1;
+
+    while (l < r) {
+        [s[l], s[r]] = [s[r], s[l]];
+        l++;
+        r--;
+    }
+
+    return s;
+};
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); // ["o","l","l","e","h"]
+console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h'])); // ["h","a","n","n","a","H"]
