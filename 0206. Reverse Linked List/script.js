4088469158b844fce8a508868fef54b103e7b3bf
msejak <7o.mseJak@gmail.com>
2023-02-28 10:05:21 +0300

    leetcode 0206

---
+++ 0206. Reverse Linked List/script.js
@@ -0,0 +1,14 @@
+const reverseList = function (head) {
+    let current = head;
+    let previous = null;
+    let next;
+
+    while (current !== null) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+
+    return previous;
+};
