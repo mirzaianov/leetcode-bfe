b952266928fc04d4fa09a032b8f813c9e7aa296c
msejak <7o.mseJak@gmail.com>
2023-03-03 10:34:10 +0300

    leetcode 0036

--- 0036. Valid Sudoku/script.js
+++ 0036. Valid Sudoku/script.js
@@ -1,3 +1,28 @@
+// const isValidSudoku = function (board) {
+//     const set = new Set();
+
+//     for (let i = 0; i < board.length; i++) {
+//         for (let j = 0; j < board[i].length; j++) {
+//             const cell = board[i][j];
+
+//             if (cell === '.') continue;
+
+//             const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+//             const row = `row: ${i}, value ${cell}`;
+//             const col = `col: ${j}, value ${cell}`;
+//             const box = `box: ${boxNum}, value ${cell}`;
+
+//             if (set.has(row) || set.has(col) || set.has(box)) return false;
+
+//             set.add(row);
+//             set.add(col);
+//             set.add(box);
+//         }
+//     }
+
+//     return true;
+// };
+
 const isValidSudoku = function (board) {
     const set = new Set();
 
@@ -7,15 +32,15 @@ const isValidSudoku = function (board) {
 
             if (cell === '.') continue;
 
-            const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
-            const row = `row: ${i}, value ${cell}`;
-            const col = `col: ${j}, value ${cell}`;
-            const box = `box: ${boxNum}, value ${cell}`;
+            let boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+            let row = `row: ${i}, cell: ${cell}`;
+            let column = `column: ${j}, cell: ${cell}`;
+            let box = `box: ${boxNum}, cell: ${cell}`;
 
-            if (set.has(row) || set.has(col) || set.has(box)) return false;
+            if (set.has(row) || set.has(column) || set.has(box)) return false;
 
             set.add(row);
-            set.add(col);
+            set.add(column);
             set.add(box);
         }
     }
