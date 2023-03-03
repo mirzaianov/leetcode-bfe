dd0cd0b40ddf3e1f0a5ea716a9a315b38a99c9ae
msejak <7o.mseJak@gmail.com>
2023-03-03 11:35:59 +0300

    leetcode 0344, 2nd time

--- 0344. Reverse String/script.js
+++ 0344. Reverse String/script.js
@@ -1,12 +1,27 @@
-const reverseString = function (s) {
-    let l = 0;
-    r = s.length - 1;
+// const reverseString = function (s) {
+//     let l = 0;
+//     r = s.length - 1;
 
-    while (l < r) {
-        [s[l], s[r]] = [s[r], s[l]];
-        l++;
-        r--;
+//     while (l < r) {
+//         [s[l], s[r]] = [s[r], s[l]];
+//         l++;
+//         r--;
+//     }
+// };
+
+// 2nd solution
+
+const reverseString = function (s) {
+    let first = 0,
+        last = s.length - 1;
+
+    while (first < last) {
+        [s[first], s[last]] = [s[last], s[first]];
+        first++;
+        last--;
     }
+
+    return s;
 };
 
 console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); // ["o","l","l","e","h"]
