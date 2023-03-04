fdf800b6aadc87ed242738a36eabf20c477c2024
msejak <7o.mseJak@gmail.com>
2023-03-04 12:01:39 +0300

    leetcode 0242, 2nd time

--- 0242. Valid Anagram/script.js
+++ 0242. Valid Anagram/script.js
@@ -1,26 +1,51 @@
+// const isAnagram = function (s, t) {
+//     if (s.length !== t.length) {
+//         return false;
+//     }
+
+//     const obj1 = {},
+//         obj2 = {};
+
+//     for (let item of s) {
+//         obj1[item] = obj1[item] + 1 || 1;
+//     }
+
+//     for (let item of t) {
+//         obj2[item] = obj2[item] + 1 || 1;
+//     }
+
+//     for (let item in obj1) {
+//         if (obj1[item] !== obj2[item]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// };
+
 const isAnagram = function (s, t) {
-    if (s.length !== t.length) {
-        return false;
-    }
+    if (s.length !== t.length) return false;
 
-    const obj1 = {},
-        obj2 = {};
+    const storeS = stringToObj(s),
+        storeT = stringToObj(t);
 
-    for (let item of s) {
-        obj1[item] = obj1[item] + 1 || 1;
-    }
-
-    for (let item of t) {
-        obj2[item] = obj2[item] + 1 || 1;
-    }
-
-    for (let item in obj1) {
-        if (obj1[item] !== obj2[item]) {
+    for (let i in storeS) {
+        if (storeS[i] !== storeT[i]) {
             return false;
         }
     }
 
     return true;
+
+    function stringToObj(string) {
+        const object = {};
+
+        for (let letter of string) {
+            object[letter] = object[letter] ? object[letter] + 1 : 1;
+        }
+
+        return object;
+    }
 };
 
 console.log(isAnagram('anagram', 'nagaram')); // true
