dafb6979956211f98a9d2827f986d515573cc5a6
msejak <7o.mseJak@gmail.com>
2023-03-05 16:33:36 +0300

    leetcode 0008

---
+++ 0008. String to Integer (atoi)/script.js
@@ -0,0 +1,50 @@
+const myAtoi = function (str) {
+    let i = 0;
+    let sign = 1;
+    let result = 0;
+
+    //Discard whitespaces in the beginning
+    while (i < str.length && str[i] == ' ') i++;
+
+    // Check if optional sign if it exists
+    if (i < str.length && (str[i] == '+' || str[i] == '-')) {
+        sign = str[i] == '-' ? -1 : 1;
+        i++;
+    }
+
+    const MAX_SAFE_32_INT = Math.pow(2, 31) - 1;
+    const MIN_SAFE_32_INT = -Math.pow(2, 31);
+
+    // Build the result and check for overflow/underflow condition
+    while (i < str.length && str[i].match(/[0-9]/) != null) {
+        const num = str[i] - '0';
+        // the second condition of this statement was hard to understand, it
+        // handles the situation where we are reaching our 32-bit boundary limit, and we need to check the very last digit.
+        if (
+            result > Math.floor(MAX_SAFE_32_INT / 10) ||
+            (result === Math.floor(MAX_SAFE_32_INT / 10) && num > MAX_SAFE_32_INT % 10)
+        ) {
+            return sign === 1 ? MAX_SAFE_32_INT : MIN_SAFE_32_INT;
+        }
+
+        // times 10 is a shift-left action
+        result = result * 10 + num;
+        i++;
+    }
+
+    return result * sign;
+};
+
+console.log(myAtoi('42')); // 42
+console.log(myAtoi('   -42')); // -42
+console.log(myAtoi('   +42')); // 42
+console.log(myAtoi('+-42')); // 0
+console.log(myAtoi('words')); // 0
+console.log(myAtoi('4193 with words')); // 4193
+console.log(myAtoi('4193 with words 555')); // 4193
+console.log(myAtoi('-2147483648')); // -2147483648
+console.log(myAtoi('-2147483650')); // -2147483648
+console.log(myAtoi('3.14159')); // 3
+console.log(myAtoi('1')); // 1
+console.log(myAtoi('   -0 123')); // 0
+console.log(myAtoi('  0000000000012345678')); // 12345678
