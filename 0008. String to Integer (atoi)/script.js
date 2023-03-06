2a926acc7d980332679a014e66275d55eae9d821
msejak <7o.mseJak@gmail.com>
2023-03-06 12:05:37 +0300

    leetcode 0008, 2nd time

--- 0008. String to Integer (atoi)/script.js
+++ 0008. String to Integer (atoi)/script.js
@@ -1,38 +1,74 @@
+// const myAtoi = function (str) {
+//     let i = 0;
+//     let sign = 1;
+//     let result = 0;
+
+//     while (i < str.length && str[i] === ' ') i++;
+
+//     // console.log(`log 1 | str: ${str} | i: ${i} | sign: ${sign} | result: ${result}`);
+
+//     if (i < str.length && (str[i] === '+' || str[i] === '-')) {
+//         sign = str[i] === '-' ? -1 : 1;
+//         i++;
+//     }
+
+//     const max32 = 2 ** 31 - 1;
+//     const min32 = -(2 ** 31);
+
+//     // console.log(`log 2 | str: ${str} | i: ${i} | sign: ${sign} | result: ${result}`);
+
+//     while (i < str.length && str[i].match(/[0-9]/) !== null) {
+//         const num = str[i] - '0';
+//         // console.log(`str[i]: ${str[i]} | num: ${num} | ${max32 % 10}`);
+
+//         if (result > Math.floor(max32 / 10) || (result === Math.floor(max32 / 10) && num > max32 % 10)) {
+//             return sign === 1 ? max32 : min32;
+//         }
+
+//         result = result * 10 + num;
+
+//         i++;
+
+//         // console.log(`log 3 | str: ${str} | i: ${i} | sign: ${sign} | result: ${result} | ${Math.floor(max32 / 10)}`);
+//     }
+
+//     if (result === 0) {
+//         return result;
+//     } else return result * sign;
+// };
+
 const myAtoi = function (str) {
-    let i = 0;
-    let sign = 1;
-    let result = 0;
+    let i = 0,
+        sign = 1,
+        result = 0;
 
-    //Discard whitespaces in the beginning
-    while (i < str.length && str[i] == ' ') i++;
+    const max = 2 ** 31 - 1,
+        min = -(2 ** 31),
+        l = str.length;
 
-    // Check if optional sign if it exists
-    if (i < str.length && (str[i] == '+' || str[i] == '-')) {
-        sign = str[i] == '-' ? -1 : 1;
+    while (i < l && str[i] === ' ') i++;
+
+    if (i < l && (str[i] === '+' || str[i] === '-')) {
+        sign = str[i] === '-' ? -1 : 1;
         i++;
     }
 
-    const MAX_SAFE_32_INT = Math.pow(2, 31) - 1;
-    const MIN_SAFE_32_INT = -Math.pow(2, 31);
+    while (i < l && str[i].match(/[0-9]/) !== null) {
+        let num = str[i] - '0';
 
-    // Build the result and check for overflow/underflow condition
-    while (i < str.length && str[i].match(/[0-9]/) != null) {
-        const num = str[i] - '0';
-        // the second condition of this statement was hard to understand, it
-        // handles the situation where we are reaching our 32-bit boundary limit, and we need to check the very last digit.
-        if (
-            result > Math.floor(MAX_SAFE_32_INT / 10) ||
-            (result === Math.floor(MAX_SAFE_32_INT / 10) && num > MAX_SAFE_32_INT % 10)
-        ) {
-            return sign === 1 ? MAX_SAFE_32_INT : MIN_SAFE_32_INT;
+        if (result > Math.floor(max / 10) || (result === Math.floor(max / 10) && num > max % 10)) {
+            return sign === 1 ? max : min;
         }
 
-        // times 10 is a shift-left action
         result = result * 10 + num;
         i++;
     }
 
-    return result * sign;
+    if (result === 0) {
+        return result;
+    } else {
+        return result * sign;
+    }
 };
 
 console.log(myAtoi('42')); // 42
@@ -43,7 +79,7 @@ console.log(myAtoi('words')); // 0
 console.log(myAtoi('4193 with words')); // 4193
 console.log(myAtoi('4193 with words 555')); // 4193
 console.log(myAtoi('-2147483648')); // -2147483648
-console.log(myAtoi('-2147483650')); // -2147483648
+console.log(myAtoi('-2147483649')); // -2147483648
 console.log(myAtoi('3.14159')); // 3
 console.log(myAtoi('1')); // 1
 console.log(myAtoi('   -0 123')); // 0
