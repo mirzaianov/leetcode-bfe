773c211afdd696b3e11ca3ad7132e04dd5093fc0
msejak <7o.mseJak@gmail.com>
2023-03-07 11:16:46 +0300

    leetcode 0014, #2

--- 0014. Longest Common Prefix/script.js
+++ 0014. Longest Common Prefix/script.js
@@ -1,10 +1,22 @@
-const longestCommonPrefix = function (strs) {
-    if (!strs.length) return '';
+// const longestCommonPrefix = function (strs) {
+//     if (!strs.length) return '';
 
+//     for (let i = 0; i < strs[0].length; i++) {
+//         for (let subs of strs) {
+//             if (subs[i] !== strs[0][i]) {
+//                 return subs.slice(0, i);
+//             }
+//         }
+//     }
+
+//     return strs[0];
+// };
+
+const longestCommonPrefix = function (strs) {
     for (let i = 0; i < strs[0].length; i++) {
-        for (let subs of strs) {
-            if (subs[i] !== strs[0][i]) {
-                return subs.slice(0, i);
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[0][i] !== strs[j][i]) {
+                return strs[j].slice(0, i);
             }
         }
     }
@@ -12,5 +24,7 @@ const longestCommonPrefix = function (strs) {
     return strs[0];
 };
 
-console.log(longestCommonPrefix(['flower', 'flow', 'flight'])); // "fl"
+console.log(longestCommonPrefix(['flower', 'flow', 'floor', 'flight'])); // "fl"
 console.log(longestCommonPrefix(['dog', 'racecar', 'car'])); // ""
+console.log(longestCommonPrefix(['', ''])); // ""
+console.log(longestCommonPrefix(['flower', 'flower', 'flower', 'flower'])); // "flower"
