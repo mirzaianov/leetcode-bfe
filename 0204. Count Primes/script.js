ebd42e15364f8ddc917680e314b7a10b643161f4
msejak <7o.mseJak@gmail.com>
2023-03-09 11:43:51 +0300

    leetcode 0204, #2

--- 0204. Count Primes/script.js
+++ 0204. Count Primes/script.js
@@ -1,21 +1,38 @@
+// const countPrimes = function (n) {
+//     const isPrime = new Array(n).fill(true);
+
+//     isPrime[0] = false;
+//     isPrime[1] = false;
+
+//     for (let i = 2; i < Math.sqrt(n); i++) {
+//         if (!isPrime[i]) continue;
+
+//         for (let j = i; j * i < n; j++) {
+//             isPrime[j * i] = false;
+//         }
+//     }
+
+//     return isPrime.reduce((count, isPrime) => {
+//         if (isPrime) count++;
+//         return count;
+//     }, 0);
+// };
+
 const countPrimes = function (n) {
-    const isPrime = new Array(n).fill(true);
+    const store = new Array(n).fill(true);
 
-    isPrime[0] = false;
-    isPrime[1] = false;
+    store[0] = false;
+    store[1] = false;
 
-    for (let i = 2; i < Math.sqrt(n); i++) {
-        if (!isPrime[i]) continue;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (store[i] === false) continue;
 
         for (let j = i; j * i < n; j++) {
-            isPrime[j * i] = false;
+            store[j * i] = false;
         }
     }
 
-    return isPrime.reduce((count, isPrime) => {
-        if (isPrime) count++;
-        return count;
-    }, 0);
+    return store.filter((item) => item === true).length;
 };
 
 console.log(countPrimes(10)); // 4
