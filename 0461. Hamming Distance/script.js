6a27a8e6f8f325bd1101de18d84c04731b8c2e53
msejak <7o.mseJak@gmail.com>
2023-03-10 11:53:14 +0300

    leetcode 0461, #2

--- 0461. Hamming Distance/script.js
+++ 0461. Hamming Distance/script.js
@@ -1,13 +1,31 @@
+// const hammingDistance = function (x, y) {
+//     let count = 0;
+//     let binary = x ^ y;
+
+//     while (binary !== 0) {
+//         if (binary & 1) count++;
+//         binary = binary >>> 1;
+//     }
+
+//     return count;
+// };
+
 const hammingDistance = function (x, y) {
-    let count = 0;
-    let binary = x ^ y;
+    return hammingWeight(x ^ y);
 
-    while (binary !== 0) {
-        if (binary & 1) count++;
-        binary = binary >>> 1;
+    function hammingWeight(n) {
+        let result = 0;
+
+        for (let i = 0; i < 32; i++) {
+            if ((n & 1) === 1) {
+                result++;
+            }
+
+            n = n >>> 1;
+        }
+
+        return result;
     }
-
-    return count;
 };
 
 console.log(hammingDistance(1, 4)); // 2
