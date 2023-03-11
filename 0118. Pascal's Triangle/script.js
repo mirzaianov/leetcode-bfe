25f4e8c331a34f9a11968c0c5c8430481db0faf2
msejak <7o.mseJak@gmail.com>
2023-03-11 18:06:35 +0300

    leetcode 0118, #2

--- 0118. Pascal's Triangle/script.js
+++ 0118. Pascal's Triangle/script.js
@@ -1,19 +1,37 @@
+// const generate = function (numRows) {
+//     const result = [];
+
+//     for (let i = 0; i < numRows; i++) {
+//         // create a new arr in result array, and add 1 as the first item
+//         result[i] = [];
+//         result[i][0] = 1;
+
+//         for (let j = 1; j < i; j++) {
+//             // this loop will only run after the second loop of i for one time on each loop. right after -> [[1],[1,1]]
+//             // in the children arr, the value of j indexed item is = prev array's left item[j-1] + right item[j]
+//             result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+//         }
+//         // finish the loop with adding 1 in the end for every child array
+//         result[i][i] = 1;
+//     }
+//     return result;
+// };
+
+// #2
+
 const generate = function (numRows) {
     const result = [];
 
     for (let i = 0; i < numRows; i++) {
-        // create a new arr in result array, and add 1 as the first item
         result[i] = [];
         result[i][0] = 1;
+        result[i][i] = 1;
 
         for (let j = 1; j < i; j++) {
-            // this loop will only run after the second loop of i for one time on each loop. right after -> [[1],[1,1]]
-            // in the children arr, the value of j indexed item is = prev array's left item[j-1] + right item[j]
             result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
         }
-        // finish the loop with adding 1 in the end for every child array
-        result[i][i] = 1;
     }
+
     return result;
 };
 
