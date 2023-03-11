ff58910c3c29f33642f5b427215a178324239634
msejak <7o.mseJak@gmail.com>
2023-03-11 17:31:23 +0300

    leetcode 0190, #2

--- 0190. Reverse Bits/script.js
+++ 0190. Reverse Bits/script.js
@@ -1,38 +1,34 @@
+// #1
+
 // const reverseBits = function (n) {
 //     let result = 0;
 
 //     for (let i = 0; i < 32; i++) {
-//         // find the last bit of n
-//         const lastBit = n & 1;
-
-//         // shift the last bit of n to the left
-//         const reversedLastBit = lastBit << (31 - i);
-
-//         // insert the reversed last bit of n into the result
-//         result |= reversedLastBit;
-
-//         // the last bit of n is already taken care of, so we need to drop it
-//         n >>>= 1;
-
+//         // take the i bit from the end then perform & with ...000001
+//         // to obtain ...000000 or ...000001 as the i bit from the end of n.
+//         const bit = (n >> i) & 1;
+//         // Perform the OR | operator with result to append the bit
+//         // at index i position by shifting left bit (31 -i)
+//         result = result | (bit << (31 - i));
 //     }
-
-//     // convert the result to an unsigned 32-bit integer
+//     // For keeping the sign for the result.
 //     return result >>> 0;
 // };
 
-const reverseBits = function (n) {
-    let result = 0;
+// #2
 
-    for (let i = 0; i < 32; i++) {
-        // take the i bit from the end then perform & with ...000001
-        // to obtain ...000000 or ...000001 as the i bit from the end of n.
-        const bit = (n >> i) & 1;
-        // Perform the OR | operator with result to append the bit
-        // at index i position by shifting left bit (31 -i)
-        result = result | (bit << (31 - i));
+const reverseBits = function (n) {
+    n = n.toString(2).split('');
+    let left = 0;
+    let right = n.length;
+
+    while (left < right) {
+        [n[left], n[right]] = [n[right], n[left]];
+        left++;
+        right--;
     }
-    // For keeping the sign for the result.
-    return result >>> 0;
+
+    return parseInt(n.join('').padEnd(32, '0'), 2);
 };
 
 console.log(reverseBits(00000010100101000001111010011100)); // 964176192
