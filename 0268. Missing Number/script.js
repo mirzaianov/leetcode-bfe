68bf023f1e0cfe940ddd1b7f536039e878bba6cb
msejak <7o.mseJak@gmail.com>
2023-03-12 13:38:16 +0300

    leetcode 0268, #2

--- 0268. Missing Number/script.js
+++ 0268. Missing Number/script.js
@@ -1,7 +1,22 @@
-const missingNumber = function (nums) {
-    const arrLength = nums.length;
+// const missingNumber = function (nums) {
+//     const arrLength = nums.length;
 
-    return (arrLength * (arrLength + 1)) / 2 - nums.reduce((acc, item) => acc + item, 0);
+//     return (arrLength * (arrLength + 1)) / 2 - nums.reduce((acc, item) => acc + item, 0);
+// };
+
+// 2
+
+const missingNumber = function (nums) {
+    const arr = [];
+
+    for (let i = 0; i <= nums.length; i++) {
+        arr.push(i);
+    }
+
+    const fullLength = arr.reduce((acc, item) => acc + item, 0);
+    const numsLength = nums.reduce((acc, item) => acc + item, 0);
+
+    return fullLength - numsLength;
 };
 
 console.log(missingNumber([3, 0, 1])); // 2
