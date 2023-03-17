6c273b7821e4c48115320ecad444d02319b930e3
msejak <7o.mseJak@gmail.com>
2023-03-17 11:18:32 +0300

    leetcode 0287

---
+++ 0287. Find the Duplicate Number/script.js
@@ -0,0 +1,16 @@
+// const findDuplicate = function (nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) return nums[i];
+//         }
+//     }
+// };
+
+// #2
+
+const findDuplicate = function (nums) {};
+
+console.log(findDuplicate([1, 1, 2, 3, 4, 5])); // 1
+console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
+console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
+console.log(findDuplicate([1, 2, 3, 4, 4])); // 4
