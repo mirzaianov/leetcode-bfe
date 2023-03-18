a4ee4c2c0d5375e38b66841390fef2cac2cf6bd3
msejak <7o.mseJak@gmail.com>
2023-03-18 16:15:50 +0300

    leetcode 0217, #2

--- 0217. Contains Duplicate/script.js
+++ 0217. Contains Duplicate/script.js
@@ -1,11 +1,27 @@
-const containsDuplicate = function (nums) {
-    const newSet = new Set();
+// const containsDuplicate = function (nums) {
+//     const newSet = new Set();
 
-    for (let item of nums) {
-        if (newSet.has(item)) {
-            return true;
-        }
-        newSet.add(item);
+//     for (let item of nums) {
+//         if (newSet.has(item)) {
+//             return true;
+//         }
+//         newSet.add(item);
+//     }
+
+//     return false;
+// };
+
+// #2
+
+const containsDuplicate = function (nums) {
+    const store = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        store[nums[i]] = store[nums[i]] ? store[nums[i]] + 1 : 1;
+    }
+
+    for (let i in store) {
+        if (store[i] > 1) return true;
     }
 
     return false;
--- 0287. Find the Duplicate Number/script.js
+++ 0287. Find the Duplicate Number/script.js
@@ -8,6 +8,34 @@
 
 // #2
 
+// const findDuplicate = function (nums) {
+//     let slow = 0,
+//         fast = 0,
+//         check = 0;
+
+//     while (true) {
+//         slow = nums[slow];
+//         fast = nums[nums[fast]];
+
+//         if (slow === fast) {
+//             break;
+//         }
+//     }
+
+//     while (true) {
+//         slow = nums[slow];
+//         check = nums[check];
+
+//         if (slow === check) {
+//             break;
+//         }
+//     }
+
+//     return check;
+// };
+
+// #3
+
 const findDuplicate = function (nums) {
     let slow = 0,
         fast = 0,
@@ -17,26 +45,21 @@ const findDuplicate = function (nums) {
         slow = nums[slow];
         fast = nums[nums[fast]];
 
-        if (slow === fast) {
-            break;
-        }
+        if (slow === fast) break;
     }
 
     while (true) {
         slow = nums[slow];
-
         check = nums[check];
 
-        if (slow === check) {
-            break;
-        }
+        if (slow === check) break;
     }
 
     return check;
 };
 
-// console.log(findDuplicate([1, 1, 2, 3, 4, 5])); // 1
-// console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
-// console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
-// console.log(findDuplicate([1, 2, 3, 4, 4])); // 4
+console.log(findDuplicate([1, 1, 2, 3, 4, 5])); // 1
+console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
+console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
+console.log(findDuplicate([1, 2, 3, 4, 4])); // 4
 console.log(findDuplicate([1, 2, 3, 4, 5, 6]));
