7596963d789e887f967fd5c8349b88ab87547b5b
msejak <7o.mseJak@gmail.com>
2023-03-19 11:46:52 +0300

    leetcode 0448, #2

--- 0448. Find All Numbers Disappeared in an Array/script.js
+++ 0448. Find All Numbers Disappeared in an Array/script.js
@@ -1,25 +1,37 @@
+// const findDisappearedNumbers = function (nums) {
+//     const accum = [];
+
+//     let i = 0;
+
+//     while (i < nums.length) {
+//         let pos = nums[i] - 1;
+
+//         if (nums[i] !== nums[pos]) {
+//             [nums[pos], nums[i]] = [nums[i], nums[pos]];
+//         } else {
+//             i++;
+//         }
+//     }
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== i + 1) {
+//             accum.push(i + 1);
+//         }
+//     }
+
+//     return accum;
+// };
+
+// #2
+
 const findDisappearedNumbers = function (nums) {
-    const accum = [];
+    const store = [];
 
-    let i = 0;
-
-    while (i < nums.length) {
-        let pos = nums[i] - 1;
-
-        if (nums[i] !== nums[pos]) {
-            [nums[pos], nums[i]] = [nums[i], nums[pos]];
-        } else {
-            i++;
-        }
+    for (let i = 1; i <= nums.length; i++) {
+        if (!nums.find((item) => item === i)) store.push(i);
     }
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== i + 1) {
-            accum.push(i + 1);
-        }
-    }
-
-    return accum;
+    return store;
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5, 6]
