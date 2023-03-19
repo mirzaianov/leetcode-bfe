b69dc06919011885bf28e314cc0b0479ec48349f
msejak <7o.mseJak@gmail.com>
2023-03-19 11:55:30 +0300

    leetcode 0136, #2

--- 0136. Single Number/script.js
+++ 0136. Single Number/script.js
@@ -1,11 +1,23 @@
-const singleNumber = function (nums) {
-    let mask = 0;
+// const singleNumber = function (nums) {
+//     let mask = 0;
 
-    for (let item of nums) {
-        mask ^= item;
+//     for (let item of nums) {
+//         mask ^= item;
+//     }
+
+//     return mask;
+// };
+
+// #2
+
+const singleNumber = function (nums) {
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        result = result ^ nums[i];
     }
 
-    return mask;
+    return result;
 };
 
 console.log(singleNumber([2, 2, 1])); // 1
