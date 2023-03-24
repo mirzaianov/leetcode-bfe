4e6f7417a2ce2af5ea2bea60fd74ec77f3991f3a
msejak <7o.mseJak@gmail.com>
2023-03-24 11:17:38 +0300

    lesson 0203, #2

--- 0203. Remove Linked List Elements/script.js
+++ 0203. Remove Linked List Elements/script.js
@@ -1,48 +1,65 @@
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-const node1 = new Node('1');
-const node2 = new Node('2');
-const node3 = new Node('3');
-const node4 = new Node('4');
-const node5 = new Node('5');
+const node1 = new ListNode('1');
+const node2 = new ListNode('2');
+const node3 = new ListNode('3');
+const node4 = new ListNode('4');
+const node5 = new ListNode('5');
 
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 
+// const removeElements = function (head, val) {
+//     if (!head) return head;
+
+//     while (head && head.val === val) {
+//         head = head.next;
+//     }
+
+//     let curr = head;
+
+//     while (curr && curr.next) {
+//         if (curr.next.val === val) {
+//             curr.next = curr.next.next;
+//         } else {
+//             curr = curr.next;
+//         }
+//     }
+
+//     return head;
+// };
+
 const removeElements = function (head, val) {
-    if (!head) return head;
+  let result = new ListNode(0);
+  result.next = head;
 
-    while (head && head.val === val) {
-        head = head.next;
+  let cur = result;
+
+  while (cur.next !== null) {
+    if (cur.next.val === val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
     }
+  }
 
-    let curr = head;
-
-    while (curr && curr.next) {
-        if (curr.next.val === val) {
-            curr.next = curr.next.next;
-        } else {
-            curr = curr.next;
-        }
-    }
-
-    return head;
+  return result.next;
 };
 
 const printLinkedList = (head) => {
-    let current = head;
+  let current = head;
 
-    while (current !== null) {
-        console.log(current.val);
-        current = current.next;
-    }
+  while (current !== null) {
+    console.log(current.val);
+    current = current.next;
+  }
 };
 
 // console.log(printLinkedList(node1));
