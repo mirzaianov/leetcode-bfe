3e561c990260fbe224eec6d08e656eff446e911e
msejak <7o.mseJak@gmail.com>
2023-03-26 21:59:58 +0300

    leetcode 0442

--- 0020. Valid Parentheses/sript.js
+++ 0020. Valid Parentheses/sript.js
@@ -18,25 +18,26 @@
 // #2
 
 const isValid = function (s) {
-    let result = [];
-    const store = {
-        ')': '(',
-        ']': '[',
-        '}': '{',
-    };
+  let result = [];
+  const store = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
 
-    for (let i = 0; i < s.length; i++) {
-        if (result.length !== 0 && result[result.length - 1] === store[s[i]]) {
-            result.pop();
-        } else {
-            result.push(s[i]);
-        }
+  for (let i = 0; i < s.length; i++) {
+    if (result.length !== 0 && result[result.length - 1] === store[s[i]]) {
+      result.pop();
+    } else {
+      result.push(s[i]);
     }
+  }
 
-    return !result.length;
+  return !result.length;
 };
 
 console.log(isValid('()')); // true
 console.log(isValid('()[]{}')); // true
 console.log(isValid('(]')); // false
+console.log(isValid('{([)]}')); // false
 console.log(isValid('[{(])[]]}')); // false
