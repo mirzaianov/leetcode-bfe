37b3d183d8590155b32e1c0f1c7d85a131611fd0
msejak <7o.mseJak@gmail.com>
2023-04-02 14:22:56 +0300

    leetcode 0258

---
+++ 0258. Add Digits/script.js
@@ -0,0 +1,21 @@
+// const addDigits = function (num) {
+//   if (num <= 9) return num;
+
+//   let toString = num
+//     .toString()
+//     .split('')
+//     .reduce((a, b) => +a + +b);
+
+//   console.log(toString);
+
+//   return addDigits(toString);
+// };
+
+const addDigits = function (num) {
+  if (num === 0) return 0;
+  else if (num % 9 === 0) return 9;
+  else return num % 9;
+};
+
+console.log(addDigits(38)); // 2
+console.log(addDigits(0)); // 0
