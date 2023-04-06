26320adf8cb15acbfe712ae5782e039cc6c083f3
msejak <7o.mseJak@gmail.com>
2023-04-06 10:39:05 +0300

    leetcode 0226

---
+++ 0226. Invert Binary Tree/script.js
@@ -0,0 +1,12 @@
+const invertTree = function (root) {
+  if (!root) return root;
+
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
