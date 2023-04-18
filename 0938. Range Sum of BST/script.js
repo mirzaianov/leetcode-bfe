cafb197b4745d37ba53de1748194ccc2bee0067a
msejak <7o.mseJak@gmail.com>
2023-04-18 12:10:41 +0300

    leetcode 0938, #2

--- 0938. Range Sum of BST/script.js
+++ 0938. Range Sum of BST/script.js
@@ -1,22 +1,22 @@
-var rangeSumBST = function (root, low, high) {
-  if (root === null) return [];
+// var rangeSumBST = function (root, low, high) {
+//   if (root === null) return [];
 
-  const values = [];
-  const queue = [root];
+//   const values = [];
+//   const queue = [root];
 
-  while (queue.length > 0) {
-    const current = queue.shift();
+//   while (queue.length > 0) {
+//     const current = queue.shift();
 
-    if (current.val >= low && current.val <= high) {
-      values.push(current.val);
-    }
+//     if (current.val >= low && current.val <= high) {
+//       values.push(current.val);
+//     }
 
-    if (current.left !== null) queue.push(current.left);
-    if (current.right !== null) queue.push(current.right);
-  }
+//     if (current.left !== null) queue.push(current.left);
+//     if (current.right !== null) queue.push(current.right);
+//   }
 
-  return values.reduce((acc, item) => acc + item, 0);
-};
+//   return values.reduce((acc, item) => acc + item, 0);
+// };
 
 const rangeSumBST = (root, low, high) => {
   if (!root) return 0;
