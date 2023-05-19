1a0fba0ceb657a2dc7593cec0908355df372bf9d
msejak <7o.mseJak@gmail.com>
2023-05-19 17:00:56 +0300

    leetcode 0054

--- 0048. Rotate Image/script.js
+++ 0048. Rotate Image/script.js
@@ -1,36 +1,34 @@
 const rotate = function (matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = i; j < matrix[0].length; j++) {
-            let temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
+  }
 
-    for (let m of matrix) {
-        let first = matrix.shift();
-        matrix.push(first.reverse());
-    }
+  for (let m of matrix) {
+    let first = matrix.shift();
+    matrix.push(first.reverse());
+  }
 
-    // return matrix; // comment this
+  // return matrix; // comment this
 };
 
 console.log(
-    rotate([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ])
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
 );
 // [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
 
 console.log(
-    rotate([
-        [5, 1, 9, 11],
-        [2, 4, 8, 10],
-        [13, 3, 6, 7],
-        [15, 14, 12, 16],
-    ])
+  rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ])
 );
 // [
 //     [15, 13,  2,  5],
