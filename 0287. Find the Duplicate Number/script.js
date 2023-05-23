0afb38b04df7a605d1da8ce129ab6f16cf9243a3
msejak <7o.mseJak@gmail.com>
2023-05-23 11:32:28 +0300

    leetcode 0287, #4

--- 0287. Find the Duplicate Number/script.js
+++ 0287. Find the Duplicate Number/script.js
@@ -36,30 +36,54 @@
 
 // #3
 
+// const findDuplicate = function (nums) {
+//     let slow = 0,
+//         fast = 0,
+//         check = 0;
+
+//     while (true) {
+//         slow = nums[slow];
+//         fast = nums[nums[fast]];
+
+//         if (slow === fast) break;
+//     }
+
+//     while (true) {
+//         slow = nums[slow];
+//         check = nums[check];
+
+//         if (slow === check) break;
+//     }
+
+//     return check;
+// };
+
+// #4
+
 const findDuplicate = function (nums) {
-    let slow = 0,
-        fast = 0,
-        check = 0;
+  let slow = nums[0];
+  let fast = nums[0];
 
-    while (true) {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
 
-        if (slow === fast) break;
-    }
+    if (slow === fast) break;
+  }
 
-    while (true) {
-        slow = nums[slow];
-        check = nums[check];
+  slow = nums[0];
 
-        if (slow === check) break;
-    }
+  while (fast !== slow) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
 
-    return check;
+  return slow;
 };
 
-console.log(findDuplicate([1, 1, 2, 3, 4, 5])); // 1
-console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
-console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
+// console.log(findDuplicate([1, 1, 2, 3, 4, 5])); // 1
+// console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
+// console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
 console.log(findDuplicate([1, 2, 3, 4, 4])); // 4
-console.log(findDuplicate([1, 2, 3, 4, 5, 6]));
+// console.log(findDuplicate([1, 2, 3, 4, 5, 6]));
+console.log(findDuplicate([2, 5, 9, 6, 9, 3, 8, 9, 7, 1])); // 9
