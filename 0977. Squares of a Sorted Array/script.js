b0852df143322c2b2411c85684f46c44f70f97b4
msejak <7o.mseJak@gmail.com>
2023-05-23 10:10:22 +0300

    leetcode 0977, #2

--- 0977. Squares of a Sorted Array/script.js
+++ 0977. Squares of a Sorted Array/script.js
@@ -1,5 +1,26 @@
+// const sortedSquares = function (nums) {
+//     return nums.map((item) => item ** 2).sort((a, b) => a - b);
+// };
+
+// #2
+
 const sortedSquares = function (nums) {
-    return nums.map((item) => item ** 2).sort((a, b) => a - b);
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  const res = new Array(n);
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (nums[left] ** 2 > nums[right] ** 2) {
+      res[i] = nums[left] ** 2;
+      left++;
+    } else {
+      res[i] = nums[right] ** 2;
+      right--;
+    }
+  }
+
+  return res;
 };
 
 console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
