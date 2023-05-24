6eaf97c47bb537fd844e42cafd8175dd0186e71d
msejak <7o.mseJak@gmail.com>
2023-05-24 10:22:53 +0300

    leetcode 0572, #2

--- 0572. Subtree of Another Tree/script.js
+++ 0572. Subtree of Another Tree/script.js
@@ -1,7 +1,25 @@
+// const isSubtree = function (root, subRoot) {
+//   if (!root) return false;
+
+//   if (isSame(root, subRoot)) return true;
+
+//   return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+// };
+
+// function isSame(a, b) {
+//   if (!a && !b) return true;
+
+//   if ((!a && b) || (a && !b) || a.val !== b.val) return false;
+
+//   return isSame(a.left, b.left) && isSame(a.right, b.right);
+// }
+
+// #2
+
 const isSubtree = function (root, subRoot) {
   if (!root) return false;
 
-  if (root.val == subRoot.val && isSame(root, subRoot)) return true;
+  if (isSame(root, subRoot)) return true;
 
   return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
