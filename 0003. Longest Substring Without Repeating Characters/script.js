504adfbd77c1d96455671e5f53fbf3f3b5cbe467
msejak <7o.mseJak@gmail.com>
2023-05-29 11:22:19 +0300

    leetcode 2703

--- 0003. Longest Substring Without Repeating Characters/script.js
+++ 0003. Longest Substring Without Repeating Characters/script.js
@@ -1,22 +1,22 @@
 const lengthOfLongestSubstring = function (s) {
-    const set = new Set();
-    let left = 0;
-    let maxSize = 0;
+  const set = new Set();
+  let left = 0;
+  let maxSize = 0;
 
-    if (s.length === 0) return 0;
-    if (s.length === 1) return 1;
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
 
-    for (let i = 0; i < s.length; i++) {
-        while (set.has(s[i])) {
-            set.delete(s[left]);
-            left++;
-        }
-
-        set.add(s[i]);
-
-        maxSize = Math.max(maxSize, i - left + 1);
+  for (let i = 0; i < s.length; i++) {
+    while (set.has(s[i])) {
+      set.delete(s[left]);
+      left++;
     }
-    return maxSize;
+
+    set.add(s[i]);
+    maxSize = Math.max(maxSize, i - left + 1);
+  }
+
+  return maxSize;
 };
 
 console.log(lengthOfLongestSubstring('abcabcbb')); // 3
