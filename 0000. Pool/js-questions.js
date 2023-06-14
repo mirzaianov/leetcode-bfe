08e19ee18402e3ae40c953b1d8426663da5327f0
msejak <7o.mseJak@gmail.com>
2023-06-14 10:51:39 +0300

    leetcode 2089

--- 0000. Pool/js-questions.js
+++ 0000. Pool/js-questions.js
@@ -14,3 +14,21 @@ console.log('lime');
 // apple, orange, lime, pineapple, melon, pear
 
 console.log('5 3 2 11'.split(' ').sort());
+
+// Console.log of the function is 'function':
+
+const myFunc = () => {};
+console.log(typeof myFunc);
+
+// При вводе имени и фамилии вернуть инициалы
+
+const myInit = (s) => {
+  return s
+    .split(' ')
+    .map((e) => `${e.slice(0, 1).toUpperCase()}.`)
+    .join('');
+};
+
+console.log(myInit('bill Gates')); // 'B.G.'
+
+//
