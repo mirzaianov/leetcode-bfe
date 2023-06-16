f1c615fa8a60d5ca1364b429ecd1c74b09be446d
msejak <7o.mseJak@gmail.com>
2023-06-16 19:27:59 +0300

    leetcode 2363, #0

--- 2363. Merge Similar Items/script.js
+++ 2363. Merge Similar Items/script.js
@@ -1,54 +1,43 @@
 var mergeSimilarItems = function (items1, items2) {
-  const res = [];
+  const res = [...items1, ...items2].sort((a, b) => a[0] - b[0]);
 
-  items1.sort((a, b) => a[0] - b[0]);
-  items2.sort((a, b) => a[0] - b[0]);
-
-  const maxItems = items1.length > items2.length ? items1 : items2;
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
-  while (maxItems[i]) {
-    if (items1[j][0] === items2[k][0]) {
-      res.push([items1[j][0], items1[j][1] + items2[k][1]]);
+  for (let i = 0; i < res.length - 1; i++) {
+    if (res[i][0] === res[i + 1][0]) {
+      res[i][1] = res[i][1] + res[i + 1][1];
+      res.splice([i + 1], 1);
     }
-
-    i++;
-    j++;
-    k++;
   }
 
   return res;
 };
 
-console.log(
-  mergeSimilarItems(
-    [
-      [1, 1],
-      [4, 5],
-      [3, 8],
-    ],
-    [
-      [3, 1],
-      [1, 5],
-    ]
-  )
-); // [[1,6],[3,9],[4,5]]
-console.log(
-  mergeSimilarItems(
-    [
-      [1, 1],
-      [3, 2],
-      [2, 3],
-    ],
-    [
-      [2, 1],
-      [3, 2],
-      [1, 3],
-    ]
-  )
-); // [[1,4],[2,4],[3,4]]
+// console.log(
+//   mergeSimilarItems(
+//     [
+//       [1, 1],
+//       [4, 5],
+//       [3, 8],
+//     ],
+//     [
+//       [3, 1],
+//       [1, 5],
+//     ]
+//   )
+// ); // [[1,6],[3,9],[4,5]]
+// console.log(
+//   mergeSimilarItems(
+//     [
+//       [1, 1],
+//       [3, 2],
+//       [2, 3],
+//     ],
+//     [
+//       [2, 1],
+//       [3, 2],
+//       [1, 3],
+//     ]
+//   )
+// ); // [[1,4],[2,4],[3,4]]
 console.log(
   mergeSimilarItems(
     [
