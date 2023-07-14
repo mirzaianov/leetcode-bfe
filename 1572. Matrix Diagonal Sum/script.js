fe2ed94293bfdaa7d9a9c8f32ee53705ecd1fca9
msejak <7o.mseJak@gmail.com>
2023-07-14 10:37:00 +0300

    leetcode 1588, #2

--- 1572. Matrix Diagonal Sum/script.js
+++ 1572. Matrix Diagonal Sum/script.js
@@ -36,7 +36,6 @@
 
 var diagonalSum = function (mat) {
   const length = mat[0].length;
-
   let sum = 0;
 
   for (let i = 0, j = length - 1; i < length; i++, j--) {
--- 1588. Sum of All Odd Length Subarrays/script.js
+++ 1588. Sum of All Odd Length Subarrays/script.js
@@ -15,22 +15,36 @@
 //     return sum;
 // };
 
+// const sumOddLengthSubarrays = function (arr) {
+//     let i = 1;
+//     let sum = 0;
+
+//     for (i = 1; i <= arr.length; i += 2) {
+//         for (let j = 0; j <= arr.length - i; j++) {
+//             for (let k = j; k < i + j; k++) {
+//                 sum += arr[k];
+//                 console.log(`[i] ${i}, [j] ${j}, [k] ${k}, [arr[k]] ${arr[k]}, [sum] ${sum}`);
+//             }
+//         }
+//     }
+
+//     return sum;
+// };
+
+// #2
+
 const sumOddLengthSubarrays = function (arr) {
-    let i = 1;
-    let sum = 0;
+  let sum = 0;
+  let len = arr.length;
 
-    for (i = 1; i <= arr.length; i += 2) {
-        for (let j = 0; j <= arr.length - i; j++) {
-            for (let k = j; k < i + j; k++) {
-                sum += arr[k];
-                console.log(`[i] ${i}, [j] ${j}, [k] ${k}, [arr[k]] ${arr[k]}, [sum] ${sum}`);
-            }
-        }
-    }
+  for (let i = 0; i < len; i++) {
+    let total = i * (len - i) + (len - i);
+    sum += Math.ceil(total / 2) * arr[i];
+  }
 
-    return sum;
+  return sum;
 };
 
 console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3])); //58
-// console.log(sumOddLengthSubarrays([1, 2])); // 3
-// console.log(sumOddLengthSubarrays([10, 11, 12])); // 66
+console.log(sumOddLengthSubarrays([1, 2])); // 3
+console.log(sumOddLengthSubarrays([10, 11, 12])); // 66
