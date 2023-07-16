02ad14adf15c36fa988bb82662d9db86a0ac074c
msejak <7o.mseJak@gmail.com>
2023-07-16 21:04:08 +0300

    leetcode 2744

--- 1614. Maximum Nesting Depth of the Parentheses/script.js
+++ 1614. Maximum Nesting Depth of the Parentheses/script.js
@@ -1,18 +1,18 @@
 const maxDepth = function (s) {
-    let maxCount = 0,
-        count = 0;
+  let maxCount = 0,
+    count = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') {
-            maxCount = Math.max(maxCount, ++count);
-        } else if (s[i] === ')') {
-            count--;
-        }
-        console.log(`s[i]: ${s[i]}, maxCount: ${maxCount}, count: ${count}`);
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      maxCount = Math.max(maxCount, ++count);
+    } else if (s[i] === ")") {
+      count--;
     }
+    console.log(`s[i]: ${s[i]}, maxCount: ${maxCount}, count: ${count}`);
+  }
 
-    return maxCount;
+  return maxCount;
 };
 
-// console.log(maxDepth('(1+(2*3)+((8)/4))+1')); // 3
-console.log(maxDepth('(1)+((2))+(((3)))')); // 3
+console.log(maxDepth("(1+(2*3)+((8)/4))+1")); // 3
+console.log(maxDepth("(1)+((2))+(((3)))")); // 3
