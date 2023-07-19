345644a9bf6c196a0f3c30cc161d75ee93c1a40c
msejak <7o.mseJak@gmail.com>
2023-07-19 10:49:10 +0300

    leetcode prettier update

--- 1812. Determine Color of a Chessboard Square/script.js
+++ 1812. Determine Color of a Chessboard Square/script.js
@@ -1,16 +1,14 @@
 const squareIsWhite = function (coordinates) {
-    let coordinates2Array = coordinates.toString().split('');
+  let coordinates2Array = coordinates.toString().split('');
 
-    const hor = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-        ver = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  const hor = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    ver = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
-    let indexOfHor = hor.indexOf(coordinates2Array[0]) + 1;
-    console.log(indexOfHor);
+  let indexOfHor = hor.indexOf(coordinates2Array[0]) + 1;
 
-    let indexOfVer = ver.indexOf(coordinates2Array[1]) + 1;
-    console.log(indexOfVer);
+  let indexOfVer = ver.indexOf(coordinates2Array[1]) + 1;
 
-    return (indexOfHor + indexOfVer) % 2 !== 0;
+  return (indexOfHor + indexOfVer) % 2 !== 0;
 };
 
 console.log(squareIsWhite('a1')); // false
