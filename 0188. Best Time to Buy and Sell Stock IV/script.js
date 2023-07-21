01fb49d05855ed035020f645ec8a57d8cb342094
msejak <7o.mseJak@gmail.com>
2023-07-21 12:04:26 +0300

    leetcode 0188

--- 0188. Best Time to Buy and Sell Stock IV/script.js
+++ 0188. Best Time to Buy and Sell Stock IV/script.js
@@ -1,14 +1,21 @@
-var maxProfit = function (prices) {
-  let [buy1, buy2, sell1, sell2] = [-Infinity, -Infinity, 0, 0];
+var maxProfit = function (k, prices) {
+  let buy = Array(k).fill(-Infinity);
+  let sell = Array(k + 1).fill(0);
 
   for (let price of prices) {
-    buy1 = Math.max(buy1, 0 - price);
-    buy2 = Math.max(buy2, sell1 - price);
-    sell1 = Math.max(sell1, buy1 + price);
-    sell2 = Math.max(sell2, buy2 + price);
+    nextBuy = Array(k).fill(-Infinity);
+    nextSell = Array(k + 1).fill(0);
+
+    for (let i = 0; i < k; i++) {
+      nextBuy[i] = Math.max(buy[i], sell[i] - price);
+      nextSell[i + 1] = Math.max(sell[i + 1], buy[i] + price);
+    }
+
+    buy = nextBuy;
+    sell = nextSell;
   }
 
-  return Math.max(sell1, sell2);
+  return Math.max(...sell);
 };
 
 console.log(maxProfit(2, [2, 4, 1])); // 2
