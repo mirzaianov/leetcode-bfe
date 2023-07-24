8bc3f7329da2dac89b41d44187a24beb26639126
msejak <7o.mseJak@gmail.com>
2023-07-24 11:02:59 +0300

    leetcode 2000, #2

--- 2000. Reverse Prefix of Word/script.js
+++ 2000. Reverse Prefix of Word/script.js
@@ -1,9 +1,26 @@
-var reversePrefix = function (word, ch) {
-  let flexString = word.slice(0, word.indexOf(ch) + 1);
-  let reversedString = [...flexString].reverse().join('');
-  let fixedString = word.slice(word.indexOf(ch) + 1);
+// var reversePrefix = function (word, ch) {
+//   let flexString = word.slice(0, word.indexOf(ch) + 1);
+//   let reversedString = [...flexString].reverse().join('');
+//   let fixedString = word.slice(word.indexOf(ch) + 1);
 
-  return reversedString + fixedString;
+//   return reversedString + fixedString;
+// };
+
+// #2
+
+var reversePrefix = function (word, ch) {
+  let start = 0;
+  let end = word.indexOf(ch);
+  const arr = word.split('');
+
+  while (start <= end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+
+    start++;
+    end--;
+  }
+
+  return arr.join('');
 };
 
 console.log(reversePrefix('abcdefd', 'd')); // "dcbaefd"
