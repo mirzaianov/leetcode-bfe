be29c66614b4e01905dafb31c4e8ee19b2f4c8fc
msejak <7o.mseJak@gmail.com>
2023-08-07 11:55:11 +0300

    leetcode 2325, #2

--- 2325. Decode the Message/scrit.js
+++ 2325. Decode the Message/scrit.js
@@ -1,51 +1,84 @@
-const decodeMessage = function (key, message) {
-    const alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z',
-    ];
+// const decodeMessage = function (key, message) {
+//     const alphabet = [
+//         'a',
+//         'b',
+//         'c',
+//         'd',
+//         'e',
+//         'f',
+//         'g',
+//         'h',
+//         'i',
+//         'j',
+//         'k',
+//         'l',
+//         'm',
+//         'n',
+//         'o',
+//         'p',
+//         'q',
+//         'r',
+//         's',
+//         't',
+//         'u',
+//         'v',
+//         'w',
+//         'x',
+//         'y',
+//         'z',
+//     ];
 
-    const keys = key2Array();
+//     const keys = key2Array();
 
-    let result = '';
+//     let result = '';
 
-    for (let i = 0; i < message.length; i++) {
-        if (message[i] === ' ') {
-            result += message[i];
-        } else {
-            result += alphabet[keys.indexOf(message[i])];
-        }
+//     for (let i = 0; i < message.length; i++) {
+//         if (message[i] === ' ') {
+//             result += message[i];
+//         } else {
+//             result += alphabet[keys.indexOf(message[i])];
+//         }
+//     }
+
+//     return result;
+
+//     function key2Array() {
+//         return Array.from(new Set(key.replace(/ /g, '')));
+//     }
+// };
+
+// #2
+
+var decodeMessage = function (key, message) {
+  const alphabet = newAlphabet(),
+    keys = key2Array(key);
+
+  let result = '';
+
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] === ' ') {
+      result += message[i];
+    } else {
+      result += alphabet[keys.indexOf(message[i])];
+    }
+  }
+
+  return result;
+
+  function newAlphabet() {
+    let newArray = [];
+
+    for (let i = 97; i <= 122; i++) {
+      newArray.push(String.fromCharCode(i));
     }
 
-    return result;
+    return newArray;
+  }
 
-    function key2Array() {
-        return Array.from(new Set(key.replace(/ /g, '')));
-    }
+  function key2Array(string) {
+    return Array.from(new Set(string.replace(/ /g, '')));
+  }
 };
 
-console.log(decodeMessage('the quick brown fox jumps over the lazy dog', 'vkbs bs t suepuv'));
-console.log(decodeMessage('eljuxhpwnyrdgtqkviszcfmabo', 'zwx hnfx lqantp mnoeius ycgk vcnjrdb'));
+console.log(decodeMessage('the quick brown fox jumps over the lazy dog', 'vkbs bs t suepuv')); // "this is a secret"
+console.log(decodeMessage('eljuxhpwnyrdgtqkviszcfmabo', 'zwx hnfx lqantp mnoeius ycgk vcnjrdb')); // "the five boxing wizards jump quickly"
