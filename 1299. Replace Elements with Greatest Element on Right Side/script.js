f2851aa20dc96a648f3e3c3b33467372fb1abe24
msejak <7o.mseJak@gmail.com>
2023-08-07 11:19:45 +0300

    leetcode 1299

---
+++ 1299. Replace Elements with Greatest Element on Right Side/script.js
@@ -0,0 +1,14 @@
+var replaceElements = function (arr) {
+  const result = new Array(arr.length);
+
+  result[arr.length - 1] = -1;
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    result[i - 1] = Math.max(arr[i], result[i]);
+  }
+
+  return result;
+};
+
+console.log(replaceElements([17, 18, 5, 4, 6, 1])); // [18,6,6,6,1,-1]
+console.log(replaceElements([400])); // [-1]
--- 2243. Calculate Digit Sum of a String/script.js
+++ 2243. Calculate Digit Sum of a String/script.js
@@ -1,24 +1,24 @@
 const digitSum = function (s, k) {
-    if (s.length <= k) {
-        return s;
+  if (s.length <= k) {
+    return s;
+  }
+
+  return digitSum(round(s, k), k);
+
+  function round(string, number) {
+    const result = [];
+
+    for (let i = 0; i < string.length; i += number) {
+      result.push(
+        string
+          .substr(i, number)
+          .split('')
+          .reduce((sum, char) => +sum + (+char || 0)),
+      );
     }
 
-    return digitSum(round(s, k), k);
-
-    function round(string, number) {
-        const result = [];
-
-        for (let i = 0; i < string.length; i += number) {
-            result.push(
-                string
-                    .substr(i, number)
-                    .split('')
-                    .reduce((sum, char) => +sum + (+char || 0))
-            );
-        }
-
-        return result.join('');
-    }
+    return result.join('');
+  }
 };
 
 console.log(digitSum('11111222223', 3)); // "135"
