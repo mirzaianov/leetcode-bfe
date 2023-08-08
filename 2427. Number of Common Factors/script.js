fa433af7a5786423b6de8ad2df4a431956f641d0
msejak <7o.mseJak@gmail.com>
2023-08-08 11:05:03 +0300

    leetcode 2427, #2

--- 2427. Number of Common Factors/script.js
+++ 2427. Number of Common Factors/script.js
@@ -1,10 +1,25 @@
+// var commonFactors = function (a, b) {
+//   let count = 1;
+//   const n = a > b ? b : a;
+
+//   for (let i = 2; i <= n; i++) {
+//     if (a % i === 0 && b % i === 0) count++;
+//   }
+
+//   return count;
+// };
+
+// #2
+
 var commonFactors = function (a, b) {
   let count = 1;
-  const n = a > b ? b : a;
 
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= a; i++) {
     if (a % i === 0 && b % i === 0) count++;
   }
 
   return count;
 };
+
+console.log(commonFactors(12, 6)); // 4
+console.log(commonFactors(25, 30)); // 2
