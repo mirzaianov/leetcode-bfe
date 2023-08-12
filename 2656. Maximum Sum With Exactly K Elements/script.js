43f4ad7b68c8819e59a2f0b1e1805710aa82303c
msejak <7o.mseJak@gmail.com>
2023-08-12 12:57:14 +0300

    leetcode 2656

--- 2656. Maximum Sum With Exactly K Elements/script.js
+++ 2656. Maximum Sum With Exactly K Elements/script.js
@@ -1,14 +1,31 @@
+// var maximizeSum = function (nums, k) {
+//   let sum = 0;
+
+//   nums.sort((a, b) => a - b);
+
+//   for (let i = 0; i < k; i++) {
+//     let cur = nums.pop();
+
+//     sum += cur;
+//     nums.push(cur + 1);
+//   }
+
+//   return sum;
+// };
+
+// #2
+
 var maximizeSum = function (nums, k) {
-  let sum = 0;
+  const max = Math.max(...nums);
 
-  nums.sort((a, b) => a - b);
+  return sum(max, 0, k);
 
-  for (let i = 0; i < k; i++) {
-    let cur = nums.pop();
+  function sum(num, curr, rep) {
+    if (rep === 0) return curr;
 
-    sum += cur;
-    nums.push(cur + 1);
+    return sum(num + 1, curr + num, rep - 1);
   }
-
-  return sum;
 };
+
+console.log(maximizeSum([1, 2, 3, 4, 5], 3)); // 18
+console.log(maximizeSum([5, 5, 5], 2)); // 11
