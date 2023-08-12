b7af28a51d513af13e2b017e0658b619df62b222
msejak <7o.mseJak@gmail.com>
2023-08-12 17:13:16 +0300

    leetcode 2704

--- 2733. Neither Minimum nor Maximum/script.js
+++ 2733. Neither Minimum nor Maximum/script.js
@@ -1,11 +1,7 @@
-var findNonMinOrMax = function(nums) {
-  
-        const set = new Set(nums.sort((a, b) => a - b));
-            
-                const arr = [...set.values()];
+var findNonMinOrMax = function (nums) {
+  const set = new Set(nums.sort((a, b) => a - b));
+  const arr = [...set.values()];
 
-
-                if (arr.length < 3) return -1;
-
-                else return arr[1];
-}
+  if (arr.length < 3) return -1;
+  else return arr[1];
+};
