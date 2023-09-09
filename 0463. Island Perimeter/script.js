60e2bf1da3bd9d6865da3534b8bf1f55b6384765
msejak <7o.mseJak@gmail.com>
2023-09-09 10:32:37 +0300

    leetcode 0463, update

--- 0463. Island Perimeter/script.js
+++ 0463. Island Perimeter/script.js
@@ -4,7 +4,10 @@ const islandPerimeter = function (grid) {
   for (let i = 0; i < grid.length; i += 1) {
     for (let j = 0; j < grid[0].length; j += 1) {
       if (i > 0 && grid[i][j] === 1 && grid[i - 1][j] === 1) {
-        count -= 1;
+        count -= 2;
+      }
+      if (j > 0 && grid[i][j] === 1 && grid[i][j - 1] === 1) {
+        count -= 2;
       }
 
       grid[i][j] === 1 ? (count += 4) : (count += 0);
