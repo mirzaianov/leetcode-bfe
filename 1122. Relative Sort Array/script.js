26e48728bdb6e2f9cb546165a54a2ed329599b26
msejak <7o.mseJak@gmail.com>
2023-09-13 23:49:24 +0300

    leetcode 1122, update

--- 1122. Relative Sort Array/script.js
+++ 1122. Relative Sort Array/script.js
@@ -1,28 +1,27 @@
-
-var relativeSortArray = function(arr1, arr2) {
+const relativeSortArray = function (arr1, arr2) {
   const map = new Map();
-      
-        for (let item of arr1) {
-            map.get(item) ? map.set(item, map.get(item) + 1) : map.set(item, 1)
-              }
-                  
-                    const newArr = [];
-                        
-                          for (let item2 of arr2) {
-                              const values = Array(map.get(item2)).fill(item2);
-                                  newArr.push(...values);
-                                      map.delete(item2);
-                                        }
-                                            
-                                              const subArr = [];
-                                                
-                                                  for (let [key, value] of map) {
-                                                      const values2 = Array(value).fill(key);
-                                                          subArr.push(...values2);
-                                                              map.delete(key);
-                                                                }
-                                                                    
-                                                                      subArr.sort((a, b) => a - b);
 
-                                                                        return [...newArr, ...subArr];
-                                                                        };
+  for (const item of arr1) {
+    map.get(item) ? map.set(item, map.get(item) + 1) : map.set(item, 1);
+  }
+
+  const newArr = [];
+
+  for (const item2 of arr2) {
+    const values = Array(map.get(item2)).fill(item2);
+    newArr.push(...values);
+    map.delete(item2);
+  }
+
+  const subArr = [];
+
+  for (const [key, value] of map) {
+    const values2 = Array(value).fill(key);
+    subArr.push(...values2);
+    map.delete(key);
+  }
+
+  subArr.sort((a, b) => a - b);
+
+  return [...newArr, ...subArr];
+};
--- 1282. Group the People Given the Group Size They Belong To/script.js
+++ 1282. Group the People Given the Group Size They Belong To/script.js
@@ -22,4 +22,4 @@ const groupThePeople = function (groupSizes) {
 };
 
 console.log(groupThePeople([3, 3, 3, 3, 3, 1, 3]));
-// console.log(groupThePeople([2, 1, 3, 3, 3, 2]));
+console.log(groupThePeople([2, 1, 3, 3, 3, 2]));
