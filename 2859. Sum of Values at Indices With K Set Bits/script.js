2f8fed690d3f653b8211c4656d5cc595b89372f2
msejak <7o.mseJak@gmail.com>
2023-09-17 21:26:22 +0300

    leetcode 2859, #2, new solution

--- 2859. Sum of Values at Indices With K Set Bits/script.js
+++ 2859. Sum of Values at Indices With K Set Bits/script.js
@@ -1,13 +1,35 @@
+// const sumIndicesWithKSetBits = function (nums, k) {
+//   const len = nums.length;
+//   let count = 0;
+
+//   for (let i = 0; i < len; i += 1) {
+//     const binIndex = i.toString(2).split('');
+
+//     const sum = binIndex.reduce((acc, curr) => acc + Number(curr), 0);
+
+//     if (sum === k) count += nums[i];
+//   }
+
+//   return count;
+// };
+
+// #2
+
 const sumIndicesWithKSetBits = function (nums, k) {
   const len = nums.length;
   let count = 0;
 
   for (let i = 0; i < len; i += 1) {
-    const binIndex = i.toString(2).split('');
+    let num = i;
+    let bits = 0;
 
-    const sum = binIndex.reduce((acc, curr) => acc + Number(curr), 0);
+    while (num) {
+      if (num & 1) bits += 1;
 
-    if (sum === k) count += nums[i];
+      num >>= 1;
+    }
+
+    if (bits === k) count += nums[i];
   }
 
   return count;
