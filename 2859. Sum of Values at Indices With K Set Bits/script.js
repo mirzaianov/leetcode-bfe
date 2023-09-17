67aa1348ce0d0ff725cfbc3f41e9abd68685437c
msejak <7o.mseJak@gmail.com>
2023-09-17 21:08:30 +0300

    leetcode 2859, #2

--- 2859. Sum of Values at Indices With K Set Bits/script.js
+++ 2859. Sum of Values at Indices With K Set Bits/script.js
@@ -1,5 +1,16 @@
-const sumOfValies = function (nums, k) {
-   //
-}
+const sumIndicesWithKSetBits = function (nums, k) {
+  const len = nums.length;
+  let count = 0;
 
-console.log(sumOfValies([5,10,1,5,2], 1)); // 13 
+  for (let i = 0; i < len; i += 1) {
+    const binIndex = i.toString(2).split('');
+
+    const sum = binIndex.reduce((acc, curr) => acc + Number(curr), 0);
+
+    if (sum === k) count += nums[i];
+  }
+
+  return count;
+};
+
+console.log(sumIndicesWithKSetBits([5, 10, 1, 5, 2], 1)); // 13
