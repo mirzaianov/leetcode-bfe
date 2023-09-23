5e2852b3eb66acfa27ded4533deef8f0c97b5b55
msejak <7o.mseJak@gmail.com>
2023-09-23 12:27:33 +0300

    leetcode 0021, #2 improvement

--- 0021. Merge Two Sorted Lists/script.js
+++ 0021. Merge Two Sorted Lists/script.js
@@ -1,33 +1,33 @@
-const mergeTwoLists = function (list1, list2) {
-  let merged = new ListNode(0);
-  let result = merged;
+// const mergeTwoLists = function (list1, list2) {
+//   let merged = new ListNode(0);
+//   let result = merged;
 
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      merged.next = new ListNode(list1.val);
-      list1 = list1.next;
-    } else {
-      merged.next = new ListNode(list2.val);
-      list2 = list2.next;
-    }
+//   while (list1 && list2) {
+//     if (list1.val < list2.val) {
+//       merged.next = new ListNode(list1.val);
+//       list1 = list1.next;
+//     } else {
+//       merged.next = new ListNode(list2.val);
+//       list2 = list2.next;
+//     }
 
-    merged = merged.next;
-  }
+//     merged = merged.next;
+//   }
 
-  while (list1) {
-    merged.next = new ListNode(list1.val);
-    list1 = list1.next;
-    merged = merged.next;
-  }
+//   while (list1) {
+//     merged.next = new ListNode(list1.val);
+//     list1 = list1.next;
+//     merged = merged.next;
+//   }
 
-  while (list2) {
-    merged.next = new ListNode(list2.val);
-    list2 = list2.next;
-    merged = merged.next;
-  }
+//   while (list2) {
+//     merged.next = new ListNode(list2.val);
+//     list2 = list2.next;
+//     merged = merged.next;
+//   }
 
-  return result.next;
-};
+//   return result.next;
+// };
 
 // #2
 
