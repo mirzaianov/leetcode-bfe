6e5cc76627976acf3779badaf97fca65a2d59321
msejak <7o.mseJak@gmail.com>
2023-09-26 16:55:59 +0300

    leetcode 0141, #3

--- 0141. Linked List Cycle/script.js
+++ 0141. Linked List Cycle/script.js
@@ -39,3 +39,21 @@ const hasCycle = (head) => {
 
   return false;
 };
+
+// #3
+
+// const hasCycle = (head) => {
+//   if (!head) return false;
+
+//   let slow = head;
+//   let fast = head.next;
+
+//   while (fast && fast.next) {
+//     if (fast === slow) return true;
+
+//     slow = slow.next;
+//     fast = fast.next.next;
+//   }
+
+//   return false;
+// }
