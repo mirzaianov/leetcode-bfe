7d3305e5f0c08fb5cc4f61f6091a3988ebd1fa57
msejak <7o.mseJak@gmail.com>
2023-10-01 12:01:31 +0300

    leetcode 0160

---
+++ 0160. Intersection of Two Linked Lists/script.js
@@ -0,0 +1,11 @@
+const getIntersectionNode = function (headA, headB) {
+  let a = headA;
+  let b = headB;
+
+  while (a !== b) {
+    a = a ? a.next : headB;
+    b = b ? b.next : headA;
+  }
+
+  return a;
+};
