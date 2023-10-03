00dad08a9d192fcbf891bcf4e92ab38f2f10145c
msejak <7o.mseJak@gmail.com>
2023-10-03 13:26:36 +0300

    leetcode 2487

---
+++ 2487. Remove Nodes From Linked List/script.js
@@ -0,0 +1,9 @@
+const removeNodes = function (head) {
+  if (!head) return null;
+
+  head.next = removeNodes(head.next);
+
+  if (head.next && head.val < head.next.val) return head.next;
+
+  return head;
+};
