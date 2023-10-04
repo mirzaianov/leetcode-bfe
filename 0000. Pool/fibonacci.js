3111a013297ad1bf72e64e02a390116c2e4a0dac
msejak <7o.mseJak@gmail.com>
2023-10-04 10:58:27 +0300

    leetcode improvement => added fibonacci + memo in the pool

--- 0000. Pool/fibonacci.js
+++ 0000. Pool/fibonacci.js
@@ -1,9 +1,17 @@
-function fibonacci(n) {
-  if (n === 0) return 0;
+// function fibonacci(n) {
+//   if (n === 0) return 0;
 
-  if (n === 1) return 1;
+//   if (n === 1) return 1;
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+function fibonacci(n, memo = { 0: 0, 1: 1 }) {
+  if (n in memo) return memo[n];
+
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+
+  return memo[n];
 }
 
 console.log(fibonacci(0)); // 0
