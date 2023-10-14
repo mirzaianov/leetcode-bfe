757ceab74b1a620da995d029622e80d4151f4094
msejak <7o.mseJak@gmail.com>
2023-10-14 17:30:50 +0300

    leetcode 0100, #2

--- 0100. Same Tree/script.js
+++ 0100. Same Tree/script.js
@@ -1,7 +1,18 @@
-const isSameTree = function (p, q) {
-  if (!p && !q) return true;
+// const isSameTree = function (p, q) {
+//   if (!p && !q) return true;
 
-  if (!p || !q || p.val !== q.val) return false;
+//   if (!p || !q || p.val !== q.val) return false;
+
+//   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+// };
+
+// #2
+
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if ((p && !q) || (!p && q)) return false;
+
+  if (p.val !== q.val) return false;
 
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
