111af864832c367111ecf3d90c591bbb80fdb009
msejak <7o.mseJak@gmail.com>
2023-11-01 11:01:03 +0300

    pool >> updated graph file

--- 0000. Pool/graph.js
+++ 0000. Pool/graph.js
@@ -74,3 +74,82 @@ const hasPath = (graph, src, dst) => {
 };
 
 console.log(hasPath(graph, 'a', 'f')); // true
+
+// undirected path
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n'],
+];
+
+const buildGraph = (edges) => {
+  const graph = {};
+
+  for (const [a, b] of edges) {
+    graph[a] ? graph[a].push(b) : (graph[a] = [b]);
+    graph[b] ? graph[b].push(a) : (graph[b] = [a]);
+  }
+
+  return graph;
+};
+
+const hasPath2 = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+
+  visited.add(src);
+
+  for (const neighbor of graph[src]) {
+    if (hasPath2(graph, neighbor, dst, visited) === true) return true;
+  }
+
+  return false;
+};
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+
+  return hasPath2(graph, nodeA, nodeB, new Set());
+};
+
+console.log(undirectedPath(edges, 'i', 'o'));
+
+// connected component count
+
+const graph2 = {
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+};
+
+const explore = (graph, node, visited) => {
+  if (visited.has(String(node))) return false;
+
+  visited.add(String(node));
+
+  for (const neighbor of graph[node]) {
+    explore(graph, neighbor, visited);
+  }
+
+  return true;
+};
+
+const connectedComponentCount = (graph) => {
+  const visited = new Set();
+  let count = 0;
+
+  for (const node in graph) {
+    if (explore(graph, node, visited) === true) count += 1;
+  }
+
+  return count;
+};
+
+console.log(connectedComponentCount(graph2)); // 2
