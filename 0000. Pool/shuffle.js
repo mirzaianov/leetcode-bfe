d88afd02fd8d235337d05a33ddc4a8bf05975e46
msejak <7o.mseJak@gmail.com>
2023-11-05 13:03:21 +0300

    pool >> updated shuffle file

--- 0000. Pool/shuffle.js
+++ 0000. Pool/shuffle.js
@@ -5,10 +5,12 @@
 // Fisher–Yates shuffle
 
 function shuffle(arr) {
-  for (let i = arr.length - 1; i > 0; i -= 1) {
-    const randIdx = Math.floor(Math.random() * (i + 1));
+  const len = arr.length;
 
-    [arr[i], arr[randIdx]] = [arr[randIdx], arr[i]];
+  for (let i = len - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
 
   return arr;
