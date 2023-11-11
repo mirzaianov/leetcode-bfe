86767c9f0b27d4d601ba835fc8fa8ea7a6ebf2a8
msejak <7o.mseJak@gmail.com>
2023-11-11 11:53:17 +0300

    leetcode >> change of the project folder structure 2

--- 02-pool/euclidean-algorithm.js
+++
@@ -1,9 +0,0 @@
-const gcd = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-
-  return gcd(b, a % b);
-};
-
-console.log(gcd(100, 550));
--- 02-pool/power.js
+++
@@ -1,7 +0,0 @@
-function power(num, exp) {
-  if (exp === 1) return num;
-
-  return num * power(num, exp - 1);
-}
-
-console.log(power(2, 5));
