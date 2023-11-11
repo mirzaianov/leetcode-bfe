fde7b6a49a99c46ac0ea031c5dc542e706af7ff5
msejak <7o.mseJak@gmail.com>
2023-11-11 17:09:51 +0300

    leetcode >> 2275

---
+++ leetcode/2275. Largest Combination With Bitwise AND Greater Than Zero.js
@@ -0,0 +1,17 @@
+const largestCombination = (candidates) => {
+  let max = -Infinity;
+
+  for (let i = 1; i <= 2 ** 31; i *= 2) {
+    let bit = 0;
+
+    for (const el of candidates) {
+      if ((el & i) !== 0) bit += 1;
+    }
+
+    max = Math.max(max, bit);
+  }
+
+  return max;
+};
+
+console.log(largestCombination([16, 17, 62, 24]));
