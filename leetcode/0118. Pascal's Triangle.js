8c1cc33900a93989fbdc5c25649c9a5d3839f4da
msejak <7o.mseJak@gmail.com>
2023-12-06 15:09:16 +0300

    fix >> corrected 0118

--- leetcode/0118. Pascal's Triangle.js
+++ leetcode/0118. Pascal's Triangle.js
@@ -39,20 +39,20 @@
 
 // #3
 
-const generate = function (numRows) {
-    const result = [];
+const generate = (numRows) => {
+  const result = [];
 
-    for (let i = 0; i < numRows; i++) {
-        result[i] = [];
-        result[i][0] = 1;
-        result[i][i] = 1;
+  for (let i = 0; i < numRows; i += 1) {
+    result[i] = [];
+    result[i][0] = 1;
+    result[i][i] = 1;
 
-        for (let j = 1; j < i; j++) {
-            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
-        }
+    for (let j = 1; j < i; j += 1) {
+      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
     }
+  }
 
-    return result;
+  return result;
 };
 
 console.log(generate(5)); // [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
