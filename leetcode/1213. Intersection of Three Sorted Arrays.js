e396cad72008e5f2d4f478b4983912a2960e7588
msejak <7o.mseJak@gmail.com>
2023-12-18 12:07:56 +0300

    leetcode >> 1213

---
+++ leetcode/1213. Intersection of Three Sorted Arrays.js
@@ -0,0 +1,23 @@
+const arraysIntersection = (arr1, arr2, arr3) => {
+  const store = new Map();
+
+  const toMap = (array, map) => {
+    for (const item of array) {
+      map.set(item, map.get(item) + 1 || 1);
+    }
+  };
+
+  toMap(arr1, store);
+  toMap(arr2, arr1);
+  toMap(arr3, arr2);
+
+  const res = [];
+
+  for (const [key, value] of store) {
+    if (value === 3) res.push(key);
+  }
+
+  return res;
+};
+
+console.log(arraysIntersection([1, 2, 3], [2, 3, 4], [3, 4, 5])); // [3]
