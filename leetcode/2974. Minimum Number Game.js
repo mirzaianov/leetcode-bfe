21a0ba349d439a485ddb32c0e51f7e5a052fdf1d
msejak <7o.mseJak@gmail.com>
2023-12-24 16:13:30 +0300

    leetcode >> 2974

---
+++ leetcode/2974. Minimum Number Game.js
@@ -0,0 +1,14 @@
+const numberGame = (nums) => {
+  nums.sort((a, b) => b - a);
+
+  const arr = [];
+
+  while (nums.length) {
+    const alice = nums.pop();
+    const bob = nums.pop();
+
+    arr.push(bob, alice);
+  }
+
+  return arr;
+};
