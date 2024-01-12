ba5912924507cdfa1a7e8499b4bd8df6bed2346f
msejak <7o.mseJak@gmail.com>
2024-01-12 11:45:05 +0300

    feat: 1897

---
+++ leetcode/1897. Redistribute Characters to Make All Strings Equal.ts
@@ -0,0 +1,19 @@
+const makeEqual = (words: string[]): boolean => {
+  const len: number = words.length;
+
+  if (len === 1) return true;
+
+  const map = new Map<string, number>();
+
+  for (const word of words) {
+    for (const letter of word) {
+      map.set(letter, map.get(letter) + 1 || 1);
+    }
+  }
+
+  for (const [key, value] of map) {
+    if (value % len !== 0) return false;
+  }
+
+  return true;
+};
