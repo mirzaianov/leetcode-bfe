1dda2a8a7b27742a355916c131797d2c68a54459
msejak <7o.mseJak@gmail.com>
2024-01-23 12:26:55 +0300

    refactor: 0561, js => ts

---
+++ leetcode/0561. Array Partition.ts
@@ -0,0 +1,9 @@
+const arrayPairSum = (nums: number[]): number => {
+  nums.sort((a, b) => a - b);
+
+  let res = 0;
+
+  for (let i = 0; i < nums.length; i += 2) res += nums[i];
+
+  return res;
+};
