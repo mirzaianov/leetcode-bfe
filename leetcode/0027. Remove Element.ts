54ea9ed5697f2828a27c7b813923b55bf94a3b54
msejak <7o.mseJak@gmail.com>
2024-01-26 12:14:04 +0300

    refactor: 0027, js => ts

---
+++ leetcode/0027. Remove Element.ts
@@ -0,0 +1,18 @@
+const removeElement = (nums: number[], val: number): number => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    if (nums[left] === val) {
+      nums[left] = nums[right];
+      right -= 1;
+    } else {
+      left += 1;
+    }
+  }
+
+  return left;
+};
+
+// console.log(removeElement([3, 2, 2, 3], 3)); // 2
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
