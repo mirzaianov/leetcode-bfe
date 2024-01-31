323499477d68fd0e698a670507c4d056e0dd47bb
msejak <7o.mseJak@gmail.com>
2024-01-31 12:30:38 +0300

    refactor: 0705, js => ts

---
+++ leetcode/0705. Design HashSet.ts
@@ -0,0 +1,27 @@
+class MyHashSet {
+  set: Record<number, number>;
+
+  constructor() {
+    this.set = {};
+  }
+
+  add(key: number): void {
+    this.set[key] = key;
+  }
+
+  remove(key: number): void {
+    delete this.set[key];
+  }
+
+  contains(key: number): boolean {
+    return this.set[key] !== undefined;
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
