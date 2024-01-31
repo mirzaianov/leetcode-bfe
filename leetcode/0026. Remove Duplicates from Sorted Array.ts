16b5824ab7ea5586f6d21ec56f8f2fcb72a41165
msejak <7o.mseJak@gmail.com>
2024-01-31 10:23:27 +0300

    refactor: 0026

--- leetcode/0026. Remove Duplicates from Sorted Array.ts
+++ leetcode/0026. Remove Duplicates from Sorted Array.ts
@@ -1,14 +1,17 @@
 function removeDuplicates(nums: number[]): number {
-  let k = 0;
+  let l = 0;
+  let r = 0;
 
-  for (let i = 0; i < nums.length; i += 1) {
-    if (nums[i] !== nums[i - 1]) {
-      nums[k] = nums[i];
-      k += 1;
+  while (r < nums.length) {
+    if (nums[r] !== nums[r - 1]) {
+      nums[l] = nums[r];
+      l += 1;
     }
+
+    r += 1;
   }
 
-  return k;
+  return l;
 }
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // 5, [0,1,2,3,4,_,_,_,_,_]
