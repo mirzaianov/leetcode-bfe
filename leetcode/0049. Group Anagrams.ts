1c5ad12e31881ea3edc742a0fa71e8239a460b9a
msejak <7o.mseJak@gmail.com>
2024-02-05 10:54:43 +0300

    feat: 0049

---
+++ leetcode/0049. Group Anagrams.ts
@@ -0,0 +1,23 @@
+const groupAnagrams = (strs: string[]): string[][] => {
+  const map = new Map<string, string[]>();
+
+  for (const str of strs) {
+    const sortedStr: string = str
+      .split('')
+      .sort((a, b) => (a > b ? -1 : 1))
+      .join('');
+
+    if (map.has(sortedStr)) {
+      const current = map.get(sortedStr)!;
+
+      map.set(sortedStr, [...map.get(sortedStr)!, str]);
+    } else {
+      map.set(sortedStr, [str]);
+    }
+  }
+
+  return [...map.values()];
+};
+
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
+console.log(groupAnagrams(['', ''])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
