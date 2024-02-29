422ad849485bdb4792dbf07b7b4aed8e1587a42b
msejak <7o.mseJak@gmail.com>
2024-02-29 10:07:22 +0300

    feat: 1038

---
+++ leetcode/1038. Binary Search Tree to Greater Sum Tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const bstToGst = (root: TreeNode | null): TreeNode | null => {
+  let sum = 0;
+
+  const dfs = (node: TreeNode | null): void => {
+    if (!node) return;
+
+    dfs(node.right);
+
+    node.val += sum;
+    sum = node.val;
+
+    dfs(node.left);
+  };
+
+  dfs(root);
+
+  return root;
+};
