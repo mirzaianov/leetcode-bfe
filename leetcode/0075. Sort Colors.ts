8bdbc247a53f9ba4dbb0b9f6f2d5320002b533e8
msejak <7o.mseJak@gmail.com>
2024-03-25 10:36:17 +0300

    refactor: 0075, added the Count Sort solution

--- leetcode/0075. Sort Colors.ts
+++ leetcode/0075. Sort Colors.ts
@@ -1,3 +1,35 @@
+// #1
+
+// const sortColors = (nums: number[]): void => {
+//   const max: number = Math.max(...nums);
+//   const counts: number[] = Array(max + 1).fill(0);
+
+//   for (const num of nums) {
+//     counts[num] += 1;
+//   }
+
+//   let startingIndex = 0;
+
+//   for (let i = 0; i < counts.length; i += 1) {
+//     const count = counts[i];
+//     counts[i] = startingIndex;
+//     startingIndex += count;
+//   }
+
+//   const sortedArray: number[] = Array(nums.length);
+
+//   for (const num of nums) {
+//     sortedArray[counts[num]] = num;
+//     counts[num] += 1;
+//   }
+
+//   for (let i = 0; i < nums.length; i += 1) {
+//     nums[i] = sortedArray[i];
+//   }
+// };
+
+// #2
+
 const sortColors = (nums: number[]): void => {
   let left = 0;
   let right = nums.length - 1;
