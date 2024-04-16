0d6d645df0fecc4eebcba1c154813c36bf6694d4
msejak <7o.mseJak@gmail.com>
2024-04-16 10:43:38 +0300

    refactor: 0094

--- leetcode/0094. Binary Tree Inorder Traversal.ts
+++ leetcode/0094. Binary Tree Inorder Traversal.ts
@@ -1,19 +1,45 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// const inorderTraversal = (root: TreeNode | null): number[] => {
+//   if (!root) return [];
+
+//   const leftValues = inorderTraversal(root.left);
+//   const rightValues = inorderTraversal(root.right);
+
+//   return [...leftValues, root.val, ...rightValues];
+// };
 
 const inorderTraversal = (root: TreeNode | null): number[] => {
   if (!root) return [];
 
-  return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+  const res: number[] = [];
+  const stack: (TreeNode | null)[] = [];
+
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop()!;
+      res.push(root.val);
+      root = root.right;
+    }
+  }
+
+  return res;
 };
+
+export default inorderTraversal;
