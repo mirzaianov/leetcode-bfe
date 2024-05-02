09ef31481a9c34401dff69378163403a1f4ca83d
mirzaianov <7o.mseJak@gmail.com>
2024-05-02 09:36:23 +0300

    refactor: 0160, js => ts

---
+++ leetcode/0160. Intersection of Two Linked Lists.ts
@@ -0,0 +1,24 @@
+class ListNode {
+  val: number;
+
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const getIntersectionNode = (headA: ListNode | null, headB: ListNode | null): ListNode | null => {
+  let a = headA;
+  let b = headB;
+
+  while (a !== b) {
+    a = a ? a.next : headB;
+    b = b ? b.next : headA;
+  }
+
+  return a;
+};
+
+export default getIntersectionNode;
