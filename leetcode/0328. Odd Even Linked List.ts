bbcb779985381f777b552cef37c66b500fee1aa5
mirzaianov <7o.mseJak@gmail.com>
2024-05-05 11:18:53 +0300

    feat: 0328

---
+++ leetcode/0328. Odd Even Linked List.ts
@@ -0,0 +1,34 @@
+class ListNode {
+  val: number;
+
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  const evenHead: ListNode | null = head.next;
+
+  let odd: ListNode = head;
+  let even: ListNode | null = head.next;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
+}
+
+export default oddEvenList;
