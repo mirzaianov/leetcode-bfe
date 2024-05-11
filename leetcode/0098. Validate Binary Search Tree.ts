4f472e79b96e4aaf15b73957bfb6008c1cd7c586
mirzaianov <7o.mseJak@gmail.com>
2024-05-11 18:38:09 +0300

    feat: 0098

---
+++ leetcode/0098. Validate Binary Search Tree.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const valid = (node: TreeNode | null, left: number, right: number): boolean => {
+  if (!node) return true;
+
+  if (node.val >= right || node.val <= left) return false;
+
+  return valid(node.left, left, node.val) && valid(node.right, node.val, right);
+};
+
+const isValidBST = (root: TreeNode | null): boolean => valid(root, -Infinity, Infinity);
+
+export default isValidBST;
