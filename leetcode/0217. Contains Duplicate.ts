e3dc9cc6851064ec745e75b2417fd870223af582
mirzaianov <7o.mseJak@gmail.com>
2024-05-21 10:30:55 +0300

    refactor: 0217

--- leetcode/0217. Contains Duplicate.ts
+++ leetcode/0217. Contains Duplicate.ts
@@ -1,15 +1,3 @@
-function containsDuplicate(nums: number[]): boolean {
-  const set = new Set<number>();
+const containsDuplicate = (nums: number[]): boolean => new Set(nums).size !== nums.length;
 
-  for (let i = 0; i < nums.length; i += 1) {
-    if (set.has(nums[i])) return true;
-
-    set.add(nums[i]);
-  }
-
-  return false;
-}
-
-console.log(containsDuplicate([1, 2, 3, 1])); // true
-console.log(containsDuplicate([1, 2, 3, 4])); // false
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
+export default containsDuplicate;
