a42830d9417ef0101531f944807d0d2897a76095
mirzaianov <7o.mseJak@gmail.com>
2024-05-24 14:25:55 +0300

    refactor: 0020

--- leetcode/0020. Valid Parentheses.ts
+++ leetcode/0020. Valid Parentheses.ts
@@ -1,31 +1,24 @@
 const isValid = (s: string): boolean => {
-  if (s.length % 2 !== 0) return false;
-
-  const store: Record<string, string> = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
-  };
+  if (s.length < 2 || s.length % 2 !== 0) return false;
 
   const stack: string[] = [];
+  const map = new Map<string, string>([
+    [')', '('],
+    [']', '['],
+    ['}', '{'],
+  ]);
 
-  for (let i = 0; i < s.length; i += 1) {
-    const currentInS: string = s[i];
-    const currentInStore: string | undefined = store[currentInS];
-    const lastInStack: string | undefined = stack[stack.length - 1];
+  for (const l of s) {
+    if (!map.has(l) || stack.at(-1) !== map.get(l)) {
+      stack.push(l);
+    }
 
-    if (stack.length !== 0 && currentInStore === lastInStack) {
+    if (stack.at(-1) === map.get(l)) {
       stack.pop();
-    } else {
-      stack.push(currentInS);
     }
   }
 
   return stack.length === 0;
 };
 
-console.log(isValid('()')); // true
-console.log(isValid('()[]{}')); // true
-console.log(isValid('(]')); // false
-console.log(isValid('{([)]}')); // false
-console.log(isValid('[{(])[]]}')); // false
+export default isValid;
