8e2abfc962bea4fd7e5ac88157fa0ccc6d3973a2
mirzaianov <7o.mseJak@gmail.com>
2024-05-27 15:52:50 +0300

    refactor: 0235, js => ts

---
+++ leetcode/0235. Lowest Common Ancestor of a Binary Search Tree.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const lowestCommonAncestor = (
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null,
+): TreeNode | null => {
+  while (root) {
+    if (p.val < root.val && q.val < root.val) root = root.left;
+    else if (p.val > root.val && q.val > root.val) root = root.right;
+    else return root;
+  }
+};
+
+export default lowestCommonAncestor;
