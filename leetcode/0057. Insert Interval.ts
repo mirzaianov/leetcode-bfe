857dc212e1af22f84e43fe4161a53e30943b0dab
mirzaianov <7o.mseJak@gmail.com>
2024-06-10 17:31:36 +0300

    feat: 0057

---
+++ leetcode/0057. Insert Interval.ts
@@ -0,0 +1,25 @@
+const insert = (intervals: number[][], newInterval: number[]): number[][] => {
+  const res: number[][] = [];
+
+  for (let i = 0; i < intervals.length; i += 1) {
+    if (newInterval[1] < intervals[i][0]) {
+      res.push(newInterval);
+      return [...res, ...intervals.slice(i)];
+    }
+
+    if (newInterval[0] > intervals[i][1]) {
+      res.push(intervals[i]);
+    } else {
+      newInterval = [
+        Math.min(newInterval[0], intervals[i][0]),
+        Math.max(newInterval[1], intervals[i][1]),
+      ];
+    }
+  }
+
+  res.push(newInterval);
+
+  return res;
+};
+
+export default insert;
