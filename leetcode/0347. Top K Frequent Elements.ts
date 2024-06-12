93dae854f4572733584cd64bbba1a154bf90e933
mirzaianov <7o.mseJak@gmail.com>
2024-06-12 22:14:17 +0300

    refactor: 0347

--- leetcode/0347. Top K Frequent Elements.ts
+++ leetcode/0347. Top K Frequent Elements.ts
@@ -1,34 +1,25 @@
-// const topKFrequent = (nums: number[], k: number): number[] => {
-//   const map = new Map<number, number>();
-
-//   for (const num of nums) {
-//     map.set(num, (map.get(num) || 0) + 1);
-//   }
-
-//   return Array.from(map.keys())
-//     .sort((a, b) => map.get(b)! - map.get(a)!)
-//     .splice(0, k);
-// };
-
-// #2
-
 const topKFrequent = (nums: number[], k: number): number[] => {
   const map = new Map<number, number>();
-  const buckets = new Array<number[]>(nums.length + 1);
-
-  for (let i = 0; i < buckets.length; i += 1) {
-    buckets[i] = [];
-  }
 
   for (const num of nums) {
     map.set(num, (map.get(num) || 0) + 1);
   }
 
-  for (const [num, count] of map.entries()) {
-    buckets[count].push(num);
+  const arr: number[][] = [];
+
+  for (const [key, value] of map) {
+    arr.push([value, key]);
   }
 
-  const flat = buckets.flat();
+  arr.sort((a, b) => b[0] - a[0]);
 
-  return flat.slice(flat.length - k);
+  const res: number[] = [];
+
+  for (let i = 0; i < k; i += 1) {
+    res.push(arr[i][1]);
+  }
+
+  return res;
 };
+
+export default topKFrequent;
