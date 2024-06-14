51b0ec2a40d348c044310c7d6137da089f7e1ae6
mirzaianov <7o.mseJak@gmail.com>
2024-06-14 13:49:21 +0300

    refactor: 0054

--- leetcode/0054. Spiral Matrix.ts
+++ leetcode/0054. Spiral Matrix.ts
@@ -43,17 +43,4 @@ const spiralOrder = (matrix: number[][]): number[] => {
   return result;
 };
 
-console.log(
-  spiralOrder([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ]),
-); // [1,2,3,6,9,8,7,4,5]
-console.log(
-  spiralOrder([
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-  ]),
-); // [1,2,3,4,8,12,11,10,9,5,6,7]
+export default spiralOrder;
