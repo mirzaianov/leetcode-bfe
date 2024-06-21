d69e35e75eb81b62019651785953f5228dad04f3
mirzaianov <7o.mseJak@gmail.com>
2024-06-21 08:49:20 +0300

    refactor: 0153

--- leetcode/0153. Find Minimum in Rotated Sorted Array.ts
+++ leetcode/0153. Find Minimum in Rotated Sorted Array.ts
@@ -1,9 +1,9 @@
 const findMin = (nums: number[]): number => {
-  const n: number = nums.length;
+  const len: number = nums.length;
   let l = 0;
-  let r: number = n - 1;
+  let r: number = len - 1;
 
-  if (n === 1 || nums[l] < nums[r]) return nums[0];
+  if (len === 1 || nums[l] < nums[r]) return nums[0];
 
   while (l < r) {
     const m: number = l + Math.floor((r - l) / 2);
