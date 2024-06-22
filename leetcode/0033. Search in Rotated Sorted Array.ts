0c230102450283be5e9ce172308e3c30d39556b2
mirzaianov <7o.mseJak@gmail.com>
2024-06-22 17:41:05 +0300

    refactor: 0033

--- leetcode/0033. Search in Rotated Sorted Array.ts
+++ leetcode/0033. Search in Rotated Sorted Array.ts
@@ -3,16 +3,22 @@ const search = (nums: number[], target: number): number => {
   let r = nums.length - 1;
 
   while (l <= r) {
-    const m = l + Math.floor((r - l) / 2);
+    const m: number = l + Math.floor((r - l) / 2);
 
     if (nums[m] === target) return m;
 
-    if (nums[l] <= nums[m]) {
-      if (nums[l] <= target && target <= nums[m]) r = m - 1;
-      else l = m + 1;
+    if (nums[m] < nums[l]) {
+      if (nums[m] <= target && target <= nums[r]) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
     } else {
-      if (nums[m] <= target && target <= nums[r]) l = m + 1;
-      else r = m - 1;
+      if (nums[l] <= target && target <= nums[m]) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
     }
   }
 
