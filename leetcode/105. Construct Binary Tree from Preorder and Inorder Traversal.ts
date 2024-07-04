a7705619d45851fb2472ab9dde8dd5ca89bb547a
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-07-04 19:12:36 +0300

    feat: 105

---
+++ leetcode/105. Construct Binary Tree from Preorder and Inorder Traversal.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const buildTree = (preorder: number[], inorder: number[]): TreeNode | null => {
+  if (!preorder.length || !inorder.length) return null;
+
+  const root: TreeNode | null = new TreeNode(preorder[0]);
+  const mid: number = inorder.indexOf(preorder[0]);
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+  return root;
+};
+
+export default buildTree;
