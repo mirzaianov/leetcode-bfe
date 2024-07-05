d2c1360d3e54328ffe1e2194cc57ea3809f9188a
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-07-05 15:23:10 +0300

    refactor: 200

--- leetcode/200. Number of Islands.ts
+++ leetcode/200. Number of Islands.ts
@@ -1,24 +1,24 @@
+const bfs = (row: number, col: number, matrix: string[][]): void => {
+  if (
+    row < 0 ||
+    col < 0 ||
+    row >= matrix.length ||
+    col >= matrix[row].length ||
+    matrix[row][col] === '0'
+  ) {
+    return;
+  }
+
+  matrix[row][col] = '0';
+
+  bfs(row, col + 1, matrix);
+  bfs(row, col - 1, matrix);
+  bfs(row + 1, col, matrix);
+  bfs(row - 1, col, matrix);
+};
+
 const numIslands = (grid: string[][]): number => {
   let count = 0;
-
-  const bfs = (row: number, col: number, matrix: string[][]): string | void => {
-    if (
-      row < 0 ||
-      col < 0 ||
-      row >= matrix.length ||
-      col >= matrix[row].length ||
-      matrix[row][col] === '0'
-    ) {
-      return;
-    }
-
-    matrix[row][col] = '0';
-
-    bfs(row, col + 1, matrix);
-    bfs(row, col - 1, matrix);
-    bfs(row + 1, col, matrix);
-    bfs(row - 1, col, matrix);
-  };
 
   for (let row = 0; row < grid.length; row += 1) {
     for (let col = 0; col < grid[row].length; col += 1) {
@@ -32,12 +32,4 @@ const numIslands = (grid: string[][]): number => {
   return count;
 };
 
-console.log(
-  numIslands([
-    ['1', '1', '1', '1', '0'],
-    ['1', '1', '0', '1', '0'],
-    ['1', '1', '0', '0', '0'],
-    ['0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0'],
-  ]),
-); // 1
+export default numIslands;
