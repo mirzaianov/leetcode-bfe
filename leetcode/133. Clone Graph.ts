8d6b17f9f35625c3c7022f4276dcf51f33ca1a55
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-07-09 09:40:34 +0300

    refactor: 133

--- leetcode/133. Clone Graph.ts
+++ leetcode/133. Clone Graph.ts
@@ -1,32 +1,34 @@
-class Node {
+class _Node {
   val: number;
 
-  neighbors: Node[];
+  neighbors: _Node[];
 
-  constructor(val?: number, neighbors?: Node[]) {
+  constructor(val?: number, neighbors?: _Node[]) {
     this.val = val === undefined ? 0 : val;
     this.neighbors = neighbors === undefined ? [] : neighbors;
   }
 }
 
-const bfs = (node: Node | null, map: Map<Node, Node>): Node | null => {
+const bfs = (node: _Node | null, map: Map<_Node, _Node>): _Node | null => {
   if (node == null) return null;
 
   if (map.has(node)) return map.get(node)!;
 
-  const newNode = new Node(node.val);
+  const newNode = new _Node(node.val);
 
   map.set(node, newNode);
 
   for (let i = 0; i < node.neighbors.length; i += 1) {
-    newNode.neighbors.push(bfs(node.neighbors[i], map));
+    newNode.neighbors.push(bfs(node.neighbors[i], map)!);
   }
 
   return newNode;
 };
 
-const cloneGraph = (node: Node | null): Node | null => {
-  const map = new Map<Node, Node>();
+const cloneGraph = (node: _Node | null): _Node | null => {
+  const map = new Map<_Node, _Node>();
 
   return bfs(node, map);
 };
+
+export default cloneGraph;
