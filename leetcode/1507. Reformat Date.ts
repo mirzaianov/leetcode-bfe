01e14c0f2fdac4b104a828b08b4c0aeeb696b094
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-08-31 10:45:13 +0300

    feat: 1507

---
+++ leetcode/1507. Reformat Date.ts
@@ -0,0 +1,25 @@
+const reformatDate = (date: string): string => {
+  const monthMap = new Map<string, string>([
+    ['Jan', '01'],
+    ['Feb', '02'],
+    ['Mar', '03'],
+    ['Apr', '04'],
+    ['May', '05'],
+    ['Jun', '06'],
+    ['Jul', '07'],
+    ['Aug', '08'],
+    ['Sep', '09'],
+    ['Oct', '10'],
+    ['Nov', '11'],
+    ['Dec', '12'],
+  ]);
+
+  const dateArr: string[] = date.split(' ');
+  const day: number = parseInt(dateArr[0]);
+  const month: string = monthMap.get(dateArr[1])!;
+  const year: string = dateArr[2];
+
+  return `${year}-${month}-${day < 10 ? '0' : ''}${day}`;
+};
+
+export default reformatDate;
