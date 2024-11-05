45074cc8f5119e8d7fe01469d7c40cb89d64c14e
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-11-05 10:40:28 +0300

    refactor: 2894 js -> ts

---
+++ leetcode/2894. Divisible and Non-divisible Sums Difference.ts
@@ -0,0 +1,16 @@
+const differenceOfSums = (n: number, m: number): number => {
+  let num1 = 0;
+  let num2 = 0;
+
+  for (let i = 1; i <= n; i += 1) {
+    if (i % m !== 0) {
+      num1 += i;
+    } else {
+      num2 += i;
+    }
+  }
+
+  return num1 - num2;
+};
+
+export default differenceOfSums;
