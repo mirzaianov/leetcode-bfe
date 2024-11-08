425963ce2dfb5c3f8ef891464d72aaf9defebc3b
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-11-08 16:26:27 +0300

    refactor: 2695 js -> ts

---
+++ leetcode/2695. Array Wrapper.ts
@@ -0,0 +1,17 @@
+class ArrayWrapper {
+  private nums: number[];
+
+  constructor(nums: number[]) {
+    this.nums = nums;
+  }
+
+  valueOf(): number {
+    return this.nums.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  toString(): string {
+    return JSON.stringify(this.nums);
+  }
+}
+
+export default ArrayWrapper;
