a55255a3e576f3cb203ef2295ff56a833ed465a4
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-12-23 12:04:20 +0300

    refactor: 905

--- leetcode/905. Sort Array By Parity.ts
+++ leetcode/905. Sort Array By Parity.ts
@@ -1,20 +1,17 @@
 const sortArrayByParity = (nums: number[]): number[] => {
-  if (nums.length < 2) return nums;
-
   let l = 0;
-  let r: number = nums.length - 1;
+  let r = 0;
 
-  while (l <= r) {
-    if (nums[l] % 2 !== 0 && nums[r] % 2 === 0) {
-      [nums[l], nums[r]] = [nums[r], nums[l]];
+  while (r < nums.length) {
+    if (nums[r] % 2 === 0) {
+      [nums[r], nums[l]] = [nums[l], nums[r]];
       l += 1;
-      r -= 1;
     }
 
-    if (nums[l] % 2 === 0) l += 1;
-
-    if (nums[r] % 2 !== 0) r -= 1;
+    r += 1;
   }
 
   return nums;
 };
+
+export default sortArrayByParity;
