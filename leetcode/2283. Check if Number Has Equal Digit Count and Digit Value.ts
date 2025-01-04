ed06555bcb4ef852caf61796ed7bb469c33cefc2
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-01-05 00:01:29 +0300

    refactor: 2283

--- leetcode/2283. Check if Number Has Equal Digit Count and Digit Value.ts
+++ leetcode/2283. Check if Number Has Equal Digit Count and Digit Value.ts
@@ -1,13 +1,17 @@
 const digitCount = (num: string): boolean => {
-  const map = new Map<string, number>();
+  const map = new Map<number, number>();
 
   for (const d of num) {
-    map.set(d, (map.get(d) || 0) + 1);
+    const curr: number = Number(d);
+
+    map.set(curr, (map.get(curr) || 0) + 1);
   }
 
   for (let i = 0; i < num.length; i += 1) {
-    if (map.get(String(i)) === Number(num[i])) continue;
-    else if (map.get(String(i)) === undefined && Number(num[i]) === 0) continue;
+    const curr: number = Number(num[i]);
+
+    if (map.get(i) === curr) continue;
+    if (map.get(i) === undefined && curr === 0) continue;
 
     return false;
   }
