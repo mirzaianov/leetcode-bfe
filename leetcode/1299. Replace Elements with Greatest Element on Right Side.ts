7b8d047f461d8e6767a4121569864da350cb5833
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-02-04 09:16:58 +0300

    refactor: 1299

--- leetcode/1299. Replace Elements with Greatest Element on Right Side.ts
+++ leetcode/1299. Replace Elements with Greatest Element on Right Side.ts
@@ -1,14 +1,14 @@
 const replaceElements = (arr: number[]): number[] => {
-  const result: number[] = new Array(arr.length);
+  const len: number = arr.length;
+  const res: number[] = new Array(len).fill(0);
 
-  result[arr.length - 1] = -1;
+  res[len - 1] = -1;
 
-  for (let i = arr.length - 1; i > 0; i -= 1) {
-    result[i - 1] = Math.max(arr[i], result[i]);
+  for (let i = len - 1; i > 0; i -= 1) {
+    res[i - 1] = Math.max(arr[i], res[i]);
   }
 
-  return result;
+  return res;
 };
 
-console.log(replaceElements([17, 18, 5, 4, 6, 1])); // [18,6,6,6,1,-1]
-console.log(replaceElements([400])); // [-1]
+export default replaceElements;
