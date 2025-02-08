ade45c4b4b5b8a3572361e0a73b487cf533572ae
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-02-08 10:52:29 +0300

    refactor: 100

--- leetcode/100. Same Tree.ts
+++ leetcode/100. Same Tree.ts
@@ -17,9 +17,9 @@ const isSameTree = (p: TreeNode | null, q: TreeNode | null): boolean => {
 
   if ((p && !q) || (!p && q)) return false;
 
-  if (p!.val !== q!.val) return false;
+  if (!p || !q || p.val !== q.val) return false;
 
-  return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right);
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
 
 export default isSameTree;
