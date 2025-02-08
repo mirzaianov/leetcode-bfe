8cb0bfdf3a8e015ab9561324da79a79b15b90269
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-02-08 10:52:39 +0300

    refactor: 70

--- leetcode/70. Climbing Stairs.ts
+++ leetcode/70. Climbing Stairs.ts
@@ -1,26 +1,26 @@
-// const climbStairs = (n: number): number => {
-//   let one = 1;
-//   let two = 1;
+// const climbStairs = (
+//   n: number,
+//   memo = new Map<number, number>([
+//     [1, 1],
+//     [2, 2],
+//   ]),
+// ): number => {
+//   if (memo.has(n)) return memo.get(n) as number;
 
-//   for (let i = 0; i < n - 1; i += 1) {
-//     [one, two] = [one + two, one];
-//   }
+//   memo.set(n, climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
 
-//   return one;
+//   return memo.get(n) as number;
 // };
 
-const climbStairs = (
-  n: number,
-  memo = new Map<number, number>([
-    [1, 1],
-    [2, 2],
-  ]),
-): number => {
-  if (memo.has(n)) return memo.get(n) as number;
+const climbStairs = (n: number): number => {
+  let one = 1;
+  let two = 1;
 
-  memo.set(n, climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
+  for (let i = 0; i < n - 1; i += 1) {
+    [one, two] = [one + two, one];
+  }
 
-  return memo.get(n) as number;
+  return one;
 };
 
 export default climbStairs;
