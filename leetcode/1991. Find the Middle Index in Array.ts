5a3b3ab3d1c6cbe6564d09827751cccf46450dca
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-06 09:43:59 +0300

    refactor: 1991 js -> ts

---
+++ leetcode/1991. Find the Middle Index in Array.ts
@@ -0,0 +1,18 @@
+const findMiddleIndex = (nums: number[]): number => {
+  let leftSum = 0;
+  let rightSum: number = nums.reduce((acc, item) => acc + item, 0);
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const curr = nums[i];
+
+    rightSum -= curr;
+
+    if (leftSum === rightSum) return i;
+
+    leftSum += curr;
+  }
+
+  return -1;
+};
+
+export default findMiddleIndex;
