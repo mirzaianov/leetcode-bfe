049cc07b638e0c9d3f5f81565ff5999b41f7eef9
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-13 09:51:09 +0300

    refactor: 1413 js -> ts

--- leetcode/1413. Minimum Value to Get Positive Step by Step Sum.ts
+++ leetcode/1413. Minimum Value to Get Positive Step by Step Sum.ts
@@ -1,6 +1,6 @@
-const minStartValue = (nums) => {
+const minStartValue = (nums: number[]): number => {
   let sum = 0;
-  let minSum = nums[0];
+  let minSum: number = nums[0];
 
   for (const num of nums) {
     sum += num;
@@ -9,3 +9,5 @@ const minStartValue = (nums) => {
 
   return minSum > 0 ? 1 : Math.abs(minSum) + 1;
 };
+
+export default minStartValue;
