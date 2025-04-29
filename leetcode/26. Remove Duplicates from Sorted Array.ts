9f93104adece484c093836d8059035c1cc47a684
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-29 09:50:46 +0300

    feat: 26

--- leetcode/26. Remove Duplicates from Sorted Array.ts
+++ leetcode/26. Remove Duplicates from Sorted Array.ts
@@ -1,4 +1,4 @@
-function removeDuplicates(nums: number[]): number {
+const removeDuplicates = (nums: number[]): number => {
   let l = 0;
   let r = 0;
 
@@ -12,8 +12,6 @@ function removeDuplicates(nums: number[]): number {
   }
 
   return l;
-}
+};
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // 5, [0,1,2,3,4,_,_,_,_,_]
-// console.log(removeDuplicates([1, 1, 2])); // 2, [1,2,_]
-// console.log(removeDuplicates([-1, 0, 0, 0, 0, 3, 3])); // 3, [-1,0,3]
+export default removeDuplicates;
