e48af6d3bf2b8b49c262fed2e5bc5a27beb936dd
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-06-09 09:58:49 +0300

    feat: 783

---
+++ leetcode/783. Minimum Distance Between BST Nodes.ts
@@ -0,0 +1,35 @@
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const minDiffInBST = (root: TreeNode | null): number => {
+  let min = Infinity;
+  let prev: TreeNode | null = null;
+
+  const dfs = (node: TreeNode | null) => {
+    if (!node) return;
+
+    dfs(node.left);
+
+    if (prev) min = Math.min(min, Math.abs(node.val - prev.val));
+
+    prev = node;
+    dfs(node.right);
+  };
+
+  dfs(root);
+
+  return min;
+};
+
+export default minDiffInBST;
