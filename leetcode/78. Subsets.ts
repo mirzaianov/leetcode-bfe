3515764e6c46f2f94dee5fc3b6272c6a05f83fc0
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-08-08 16:51:17 +0300

    refactor: 78

--- leetcode/78. Subsets.ts
+++ leetcode/78. Subsets.ts
@@ -2,20 +2,20 @@ const subsets = (nums: number[]): number[][] => {
   const res: number[][] = [];
   const curr: number[] = [];
 
-  const buildSubsets = (i: number): void => {
-    if (i === nums.length) {
-      res.push(curr.slice());
+  const backtrack = (i: number): void => {
+    if (nums.length === i) {
+      res.push([...curr]);
       return;
     }
 
     curr.push(nums[i]);
-    buildSubsets(i + 1);
+    backtrack(i + 1);
 
     curr.pop();
-    buildSubsets(i + 1);
+    backtrack(i + 1);
   };
 
-  buildSubsets(0);
+  backtrack(0);
 
   return res;
 };
