0a5f37488ca7d7fb281e3c56fa5c017f6866f2cd
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-08-17 12:36:57 +0300

    refactor: 118

--- leetcode/118. Pascal's Triangle.ts
+++ leetcode/118. Pascal's Triangle.ts
@@ -1,15 +1,17 @@
 const generate = (numRows: number): number[][] => {
-  const result: number[][] = [];
+  const res: number[][] = [];
 
   for (let i = 0; i < numRows; i += 1) {
-    result[i] = [];
-    result[i][0] = 1;
-    result[i][i] = 1;
+    res[i] = [];
+    res[i][0] = 1;
+    res[i][i] = 1;
 
     for (let j = 1; j < i; j += 1) {
-      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+      res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
     }
   }
 
-  return result;
+  return res;
 };
+
+export default generate;
